eaee4eadb62843624b44e02e5db7d68396504954
arvindr9 <saramaswami@gmail.com>
2018-01-02 09:20:50 -0800

    Changed the AJAX request such that it imports and displays the form from a separate link. Also included a render button

--- scripts/src/admin/FormAdminPage.tsx
+++ scripts/src/admin/FormAdminPage.tsx
@@ -5,31 +5,42 @@ import FormPage from "../form/FormPage";
 class FormAdminPage extends React.Component<any, any> {
     constructor(props:any) {
         super(props);
+        this.getFormList = this.getFormList.bind(this);
         this.state = {
             formList: [],
             center: "CMSJ"
         }
     }
 
+    getFormList(url) {
+        var that = this;
+        axios.get(url, {"responseType": "json"})
+        .then((response) => {
+            console.log(response.data);
+            that.setState({formList: response.data.res});
+        })
+    }
+
     componentDidMount() {
         let formListUrl = 'https://ajd5vh06d8.execute-api.us-east-2.amazonaws.com/dev/gcmw-cff-render-form?action=renderForm&id=59dbf12b734d1d18c05ebd21';
 
-        axios.get(formListUrl, {"responseType": "json",
-                                "headers": {"Access-Control-Request-Headers": "access-control-allow-origin"}})
-        .then((response) => {
-            console.log(response.data)
-        })
+        //axios.get(formListUrl, {"responseType": "json"})
+        //.then((response) => {
+        //    console.log(response.data)
+        //});
         this.setState({
             formList: [
             {"id": "129318954835", "name": "mah form"},
             {"id": "465738945468", "name": "moo"}
         ]
         });
+        this.getFormList(formListUrl);
     }
     render() {
         return (
         <div className="App FormAdminPage">
             <h1>GCMW Form Admin - {this.state.center}</h1>
+            <button>Render</button>
             <table>
                 <tbody>
                     {this.state.formList.map((form) =>
