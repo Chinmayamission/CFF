e62d00dcfbca691d749547d9dcdd1a4f9b6bc8e0
arvindr9 <saramaswami@gmail.com>
2018-01-01 16:06:41 -0800

    started the page that displays forms

--- scripts/src/admin/FormAdminPage.tsx
+++ scripts/src/admin/FormAdminPage.tsx
@@ -1,22 +1,39 @@
 import * as React from 'react';
-import FormPage from "../form/FormPage";
+//import FormPage from "../form/FormPage";
 
-var This;
 class FormAdminPage extends React.Component<any, any> {
     constructor(props:any) {
         super(props);
-        This = this;
         this.state = {
-            center: {
-                "name": "CMSJ"
-            }
+            formList: [],
+            center: "CMSJ"
         }
+    }
+
+    componentDidMount() {
+        this.setState({
+            formList: [
+            {"id": "129318954835", "name": "mah form"},
+            {"id": "465738945468", "name": "moo"}
+        ]
+        });
     }
     render() {
         return (
         <div className="App FormAdminPage">
-            <h1>GCMW Form Admin - {this.state.center.name}</h1>
-            <FormPage formId={"5a3bdfd5059638058c8ef478"} />
+            <h1>GCMW Form Admin - {this.state.center}</h1>
+            <table>
+                <tbody>
+                    {this.state.formList.map((form) =>
+                        <tr key = {form["id"]}>
+                            <td style = {{border: '1px solid black'}}>{form["name"]}</td>
+                            <td><button>View</button></td>
+                            <td><button>Edit</button></td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+            {/*<FormPage formId={"5a3bdfd5059638058c8ef478"} />*/}
         </div>
         );
     }
--- scripts/src/index.tsx
+++ scripts/src/index.tsx
@@ -1,4 +1,3 @@
-/// <reference path="./interfaces.d.ts"/>
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 // import App from './App';
