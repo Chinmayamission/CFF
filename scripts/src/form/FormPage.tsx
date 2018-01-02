9aa177a928d02ec027d51f06cfc013993355335b
arvindr9 <saramaswami@gmail.com>
2018-01-02 12:14:46 -0800

    removed large block of code from FormPage

--- scripts/src/form/FormPage.tsx
+++ scripts/src/form/FormPage.tsx
@@ -201,51 +201,6 @@ class FormPage extends React.Component<IFormPageProps, IFormPageState> {
 
   componentDidMount() {    
     let formId = this.props.formId['$oid'];
-    //let schemaModifiersUrl = "http://registration.chinmayamission.com/forms/" + formId + "/data/schemaWithModifiers";
-    //schemaModifiersUrl = "http://www.chinmayamissionalpharetta.org/wp-json/";
-    //axios.get(schemaModifiersUrl, {"responseType": "json"})
-    //.then((response) => {
-    //  return {
-    //    "schema": {
-    //      "title": "My form!",
-    //      type: "object",
-    //      properties: {
-    //        bar: {type: "string"},
-    //        name: {
-    //          "type": "object",
-    //          "properties": {
-    //            "first": {"type": "string"},
-    //            "last": {"type": "string"}
-    //          }
-    //        }
-    //      }
-    //    },
-    //    "schemaModifiers": {
-    //      "name": {
-    //        "first": {
-    //          "ui:widget": "textarea"
-    //        }
-    //      },
-    //      "title": "Form 2"
-    //    }
-    //  }
-    //  // return response.data
-    //})
-    //.then(this.unescapeJSON)
-    //.then((data) => {
-    //  var options = data["schemaModifiers"];
-    //  var uiSchema = options;
-    //  var schema = data["schema"];
-    //  console.log(schema);
-    //  schema = deref(schema);
-    //  console.log(schema);
-    //  this.populateSchemaWithOptions(schema["properties"], options);
-    //  this.filterUiSchema(uiSchema);
-    //  console.log(options, uiSchema, schema);
-    //    
-    //  This.setState({ uiSchema: uiSchema, schema: schema, status: STATUS_FORM_RENDERED });
-    //  
-    //});
     let endpoint = 'https://ajd5vh06d8.execute-api.us-east-2.amazonaws.com/prod/gcmw-cff-render-form';
     let apiKey = 'test';
     let formListUrl = endpoint + "?action=formRender" + "&id=" + formId;
