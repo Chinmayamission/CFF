8656ebc04f1f426e9e30c16190e95a8603b283a2
arvindr9 <saramaswami@gmail.com>
2018-01-03 10:35:18 -0800

    transposed the table

--- scripts/src/admin/ResponseTable.tsx
+++ scripts/src/admin/ResponseTable.tsx
@@ -4,6 +4,22 @@ import axios from 'axios';
 const STATUS_RESPONSES_LOADING = 0;
 const STATUS_RESPONSES_RENDERED = 2;
 
+function ObjectRow(props) {
+    console.log("hi2");
+    console.log(props.row);
+    let row = props.row;
+    let ret = (
+        <tr style = {{outline: 'thin solid'}}>
+            {row.map((info) => {
+                console.log(info);
+                return (<td key = {info}>{info}</td>)
+            })}
+        </tr>
+    )
+    console.log("ret\n", ret);
+    return ret;
+}
+
 
 class ResponseTable extends React.Component<any, any> {
     constructor(props:any) {
@@ -12,6 +28,39 @@ class ResponseTable extends React.Component<any, any> {
             status: STATUS_RESPONSES_LOADING,
             tableData: {}
         }
+    }
+
+    createTable(tableData) {
+        console.log("hi");
+        let len = this.findLen(tableData);
+        let rows = [];
+        let currRow = [];
+        for(let key in tableData) {
+            currRow.push(key);
+        }
+        console.log("currRpw", currRow);
+        rows.push(<ObjectRow key = {len} row = {currRow}/>);
+        for(let i = 0; i < len; i++) {
+            currRow = []
+            for(let key in tableData) {
+                currRow.push(tableData[key][i]);   
+            }
+            console.log("currRpw", currRow);
+            rows.push(<ObjectRow key = {i} row = {currRow}/>);
+        }
+
+
+        return <table><tbody>{rows}</tbody></table>;
+    }
+
+    findLen(tableData) {
+        let len = 0;
+        for(let key in tableData) {
+            if(tableData[key]) {
+                return tableData[key].length;
+            }
+        }
+        return 0;
     }
 
     getFormUrl(action) {
@@ -48,6 +97,7 @@ class ResponseTable extends React.Component<any, any> {
     }
 
 
+
     componentDidMount() {
         const responseUrl = this.getFormUrl('formResponses');
         console.log(responseUrl);
@@ -75,6 +125,8 @@ class ResponseTable extends React.Component<any, any> {
           } else if (this.state.status == STATUS_RESPONSES_RENDERED) {
             return (
                 <div>
+                    
+                    {this.createTable(this.state.tableData)}
                     <table>
                         <tbody>
                         {Object.keys(this.state.tableData).map( (key, i) => 
