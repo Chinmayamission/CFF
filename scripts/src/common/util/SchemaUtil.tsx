34d05c27503c57dc83a341a0723ff1702e0e3f71
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-01-06 20:28:55 -0500

    small bug in schema path -- items

--- scripts/src/common/util/SchemaUtil.tsx
+++ scripts/src/common/util/SchemaUtil.tsx
@@ -3,11 +3,12 @@ export module SchemaUtil {
         /* Converts a schemaModifier / data fieldPath to a schemaPath
          * replaces "a.0.b.c" => "a.0.properties.b.properties.c" => "a.items.properties.b.properties.c"
          * "race.enum.2" => "race.properties.enum.2" => "race.properties.enum.2" => "race.enum.2"
+         * participants.items.race.enum.2 => participants.items.properties.race.enum.2
          */
         return fieldPath
             .replace(/\.([^\d])/g,".properties.$1")
             .replace(/\.\d*\./g, ".items.")
-            .replace(/\.properties\.(enum|enumValues|title|description)/g, ".$1") // don't add "properties" before certain predefined keys of schema, as they aren't custom object properties.
+            .replace(/\.properties\.(enum|enumValues|title|description|items)/g, ".$1") // don't add "properties" before certain predefined keys of schema, as they aren't custom object properties.
     }
     export function objToSchemaModifierPath(fieldPath) {
         /* Converts a schema path to a schemaModifier / uiSchema path by removing ".properties." from it,
