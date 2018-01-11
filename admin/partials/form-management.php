a27739dea26d38b8edd12b34edb61e59082bfe7e
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-01-11 11:18:37 -0800

    fix typo

--- admin/partials/form-management.php
+++ admin/partials/form-management.php
@@ -11,4 +11,4 @@ $ccmt_cff_api_endpoint = $ccmt_cff_options['ccmt_cff_api_endpoint'];
 <a href="<?php echo $ccmt_cff_options_page_url; ?>">Set up API key</a>
 <div id="ccmt-cff-admin"
     data-ccmt-cff-api-key="<?php echo $ccmt_cff_api_key; ?>"
-    data-ccmt-cff-api-endpoint="<?php echo $ccmt_cff_api_endpoint; ?>></div>
+    data-ccmt-cff-api-endpoint="<?php echo $ccmt_cff_api_endpoint; ?>"></div>
