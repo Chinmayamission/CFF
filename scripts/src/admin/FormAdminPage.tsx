0a852177c492e8e2ef8d43c6df2fb9df9051dcde
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-04-21 09:22:10 -0700

    footer, clean up code

--- scripts/src/admin/FormAdminPage.tsx
+++ scripts/src/admin/FormAdminPage.tsx
@@ -18,16 +18,11 @@ import { withAuthenticator } from 'aws-amplify-react';
 import { Auth, API } from 'aws-amplify';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
+declare var VERSION: string;
 const STATUS_LOADING = 0;
 const STATUS_ERROR = 11;
 const STATUS_ACCESS_DENIED = 21;
 const STATUS_CENTER_LIST = 31;
-const STATUS_FORM_LIST = 41;
-const STATUS_FORM_RENDER = 51; // Not used.
-const STATUS_FORM_RESPONSES = 61;
-const STATUS_FORM_RESPONSE_SUMMARY = 62;
-const STATUS_FORM_EMBED = 71;
-const STATUS_FORM_EDIT = 81;
 
 class FormAdminPage extends React.Component<IFormAdminPageProps, IFormAdminPageState> {
     constructor(props:any) {
@@ -44,43 +39,6 @@ class FormAdminPage extends React.Component<IFormAdminPageProps, IFormAdminPageS
             apiKey: null,
             loading: false
         }
-    }
-
-    editForm(form) {
-        this.setState({
-            selectedForm: form,
-            status: STATUS_FORM_EDIT
-        });
-    }
-
-    embedForm(form) {
-        this.setState({
-            selectedForm: form,
-            status: STATUS_FORM_EMBED
-        })
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        /*let stateKeysToEncode = ["selectedForm.name", "selectedForm.center", "selectedForm.id", "center", "status"];
-        if (pick(this.state, stateKeysToEncode) != pick(prevState, stateKeysToEncode)) {
-            let encodedState = flatten(pick(this.state, stateKeysToEncode));
-            let newQS = queryString.stringify(encodedState);
-            window.location.hash = newQS;//queryString.stringify(encodedState);   
-        }*/
-    }
-
-
-    loadResponses(form) {
-        this.setState({
-            selectedForm: form,
-            status: STATUS_FORM_RESPONSES
-        });
-    }
-    loadResponseSummary(form) {
-        this.setState({
-            selectedForm: form,
-            status: STATUS_FORM_RESPONSE_SUMMARY
-        })
     }
     componentWillReceiveProps(nextProps) {
         console.log(nextProps, nextProps.authState);
@@ -153,7 +111,7 @@ class FormAdminPage extends React.Component<IFormAdminPageProps, IFormAdminPageS
             return <Loading hasError={true} />;
         }
         return (<Router>
-            <div>
+            <div className="App FormAdminPage">
             {this.state.user.id &&
                 <Switch>
                     <Route path="/" exact render={(props) =>
@@ -171,6 +129,11 @@ class FormAdminPage extends React.Component<IFormAdminPageProps, IFormAdminPageS
                 <FormList key={props.match.params.centerSlug} onError={e => this.onError(e)} userId={this.state.user.id} {...props} />
             }/>
             <Route path="/:centerSlug/:centerId/:formId" component={FormPages} />
+            <footer className="ccmt-cff-admin-footer">
+                <div className="container">
+                    <span className="text-muted">Chinmaya Forms Framework, version {VERSION}</span>
+                </div>
+            </footer>
             </div>
         </Router>);
 
@@ -204,49 +167,5 @@ function AccessDenied(props) {
             <pre className="cff-copy-box">{props.userId}</pre>
     </div>);
 }
-/*
-class Other {
-    render2() {
-        if (this.state.status == STATUS_LOADING) {
-            return <Loading hasError={this.state.hasError} />;
-        }
-
-        return (
-        <div className="App FormAdminPage">
-            <h1>CCMT Form Admin - {this.state.center && this.state.center.name}</h1>
-            <p>User id: {this.state.userId}</p>
-            Change center:
-            <select className="form-control" value={this.state.center.id} onChange={(e) => {
-                let selectedCenter = find(this.state.centerList, {"id": parseInt(e.target.value)});
-                this.setState({center: selectedCenter, status: STATUS_LOADING}, this.loadFormList);
-            }}>
-            {this.state.centerList.map(e => <option key={e.id} value={e.id}>{e.name}</option>)}
-            </select>
-            {this.state.status != STATUS_FORM_LIST && 
-                <a href="#" onClick={() => {window.location.hash=""; this.loadFormList(); } }>Back to form list</a>
-            }
-            {this.state.status != STATUS_ACCESS_DENIED && this.state.formList && <FormList
-                apiEndpoint={this.props.apiEndpoint}
-                apiKey={this.state.apiKey}
-                editForm = {(e) => this.editForm(e)}
-                embedForm = {(e) => this.embedForm(e)}
-                loadResponses= {(e) => this.loadResponses(e)} 
-                loadResponseSummary = {(e) => this.loadResponseSummary(e)}
-                formList = {this.state.status == STATUS_FORM_LIST ? this.state.formList : [this.state.selectedForm]} />}
-            {this.state.status == STATUS_FORM_EMBED && 
-                <FormEmbed form={this.state.selectedForm} apiEndpoint={this.props.apiEndpoint} />
-            }
-            {this.state.status == STATUS_FORM_EDIT && 
-                <FormEdit form={this.state.selectedForm} apiEndpoint={this.props.apiEndpoint} apiKey={this.state.apiKey} />
-            }
-            {this.state.status == STATUS_FORM_RESPONSES &&
-                <ResponseTable form={this.state.selectedForm} apiEndpoint={this.props.apiEndpoint} apiKey={this.state.apiKey}
-                    handleError={(e) => this.handleError(e)} />
-            }
-        </div>
-        );
-    }
-}
-*/
 
 export default withAuthenticator(FormAdminPage, { includeGreetings: true });
