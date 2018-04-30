80d3d30abe6fdce4de27fbcf71b47609a09a1418
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-04-30 11:04:13 -0700

    add integration tests for admin response edit

--- ccmt-cff-rest-api/tests/integration/test_formSubmit.py
+++ ccmt-cff-rest-api/tests/integration/test_formSubmit.py
@@ -7,6 +7,7 @@ from chalice.local import LocalGateway
 import json
 from .constants import FORM_ID, FORM_DATA_ONE, FORM_SUBMIT_RESP_ONE, FORM_DATA_TWO, FORM_SUBMIT_RESP_TWO
 from app import app
+from pydash.objects import set_
 
 class FormSubmit(unittest.TestCase):
     def setUp(self):
@@ -22,3 +23,23 @@ class FormSubmit(unittest.TestCase):
         body = json.loads(response['body'])
         responseId = body['res'].pop("id")
         self.assertEqual(body['res'], FORM_SUBMIT_RESP_ONE)
+        """View response."""
+        response = self.lg.handle_request(method='GET',
+                                          path='/forms/{}/responses/{}/view'.format(FORM_ID, responseId),
+                                          headers={},
+                                          body='')
+        self.assertEqual(response['statusCode'], 200, response)
+        body = json.loads(response['body'])
+        self.assertEqual(body['res']['value'], FORM_DATA_ONE['data'])
+        
+        """Edit response."""
+        body = {"path": "value.contact_name.last", "value": "NEW_LAST!"}
+        response = self.lg.handle_request(method='POST',
+                                          path='/forms/{}/responses/{}/edit'.format(FORM_ID, responseId),
+                                            headers={"Content-Type": "application/json"},
+                                          body=json.dumps(body))
+        expected_data = dict(FORM_DATA_ONE["data"])
+        set_(expected_data, "contact_name.last", "NEW_LAST!")
+        self.assertEqual(response['statusCode'], 200, response)
+        body = json.loads(response['body'])
+        self.assertEqual(body['res']['value'], expected_data)
