f88b4c11585c72f10039d38d69b6166e2d0a5682
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-05-20 07:18:27 -0700

    allow form render with schema/uischema

--- ccmt-cff-rest-api/chalicelib/routes/formRender.py
+++ ccmt-cff-rest-api/chalicelib/routes/formRender.py
@@ -19,16 +19,21 @@ def form_render(formId):
     Todo: get coupon codes."""
     form = TABLES.forms.get_item(
         Key=dict(id=formId, version=1),
-        ProjectionExpression="#name, id, version, date_created, date_last_modified, #schema, schemaModifier",
+        # todo: need to return schemaModifier.
+        ProjectionExpression="#name, id, version, date_created, date_last_modified, #schema, uiSchema, schemaModifier, formOptions",
         ExpressionAttributeNames={"#name": "name", "#schema": "schema"}
     )["Item"]
-    form["schema"] = TABLES.schemas.get_item(
-        Key=form["schema"]
-    )["Item"]
-    form["schemaModifier"] = TABLES.schemaModifiers.get_item(
-        Key=form["schemaModifier"]
-    )["Item"]
-    if app.current_request.query_params and "versions" in app.current_request.query_params:
-        form["schema_versions"] = get_versions(TABLES.schemas, form["schema"]["id"])
-        form["schemaModifier_versions"] = get_versions(TABLES.schemas, form["schema"]["id"])
+    if "uiSchema" in form:
+        # Everything is good here.
+        pass
+    else:
+        form["schema"] = TABLES.schemas.get_item(
+            Key=form["schema"]
+        )["Item"]
+        form["schemaModifier"] = TABLES.schemaModifiers.get_item(
+            Key=form["schemaModifier"]
+        )["Item"]
+        if app.current_request.query_params and "versions" in app.current_request.query_params:
+            form["schema_versions"] = get_versions(TABLES.schemas, form["schema"]["id"])
+            form["schemaModifier_versions"] = get_versions(TABLES.schemas, form["schemaModifier"]["id"])
     return {'res': form }
--- ccmt-cff-rest-api/tests/integration/test_formRender.py
+++ ccmt-cff-rest-api/tests/integration/test_formRender.py
@@ -36,4 +36,17 @@ class FormRender(unittest.TestCase):
         self.assertIn("value", body['res']['schemaModifier'])
         self.assertIn("schema_versions", body['res'])
         self.assertIn("schemaModifier_versions", body['res'])
-        
+    def test_render_form_uischema_schema(self):
+        formId = "e211731b-97f4-40ff-8ff6-9658d711d1a0"
+        response = self.lg.handle_request(method='GET',
+                                          path='/forms/{}/render'.format(formId),
+                                          headers={},
+                                          body='')
+        self.assertEqual(response['statusCode'], 200, response)
+        body = json.loads(response['body'])
+        EXPECTED = {'version': 1.0, 'uiSchema': {'phone': {'ui:widget': 'phone', 'ui:placeholder': 'Phone Number'}}, 'schema': {'description': 'Contact form.', 'ui:order': ['contact_name', 'email', 'phone', 'area_expertise', 'books_authored', 'papers_authored'], 'title': 'Form with uiSchema and schema inline (Academicians Contact Form)', 'type': 'object', 'definitions': {'name': {'ui:order': ['first', 'last'], 'title': ' ', 'type': 'object', 'properties': {'last': {'title': 'Last Name', 'type': 'string', 'classNames': 'twoColumn'}, 'first': {'title': 'First Name', 'type': 'string', 'classNames': 'twoColumn'}}}}, 'properties': {'area_expertise': {'type': 'string'}, 'contact_name': {'ui:order': ['first', 'last'], 'title': 'Contact Name', 'type': 'object', 'properties': {'last': {'ui:cff:nonModifiable': True, 'classNames': 'twoColumn', 'title': 'Last Name', 'type': 'string'}, 'first': {'ui:cff:nonModifiable': True, 'classNames': 'twoColumn', 'title': 'First Name', 'type': 'string'}}}, 'papers_authored': {'type': 'array', 'items': {'type': 'string'}}, 'phone': {'title': 'Phone Number', 'type': 'string'}, 'books_authored': {'type': 'array', 'items': {'type': 'string'}}, 'email': {'ui:cff:nonModifiable': True, 'format': 'email', 'type': 'string'}}}, 'date_last_modified': '2018-05-20T04:16:30.579101', 'formOptions': {}, 'date_created': '2018-05-20T03:59:32.211390', 'id': 'e211731b-97f4-40ff-8ff6-9658d711d1a0', 'name': 'uischema and schema form -- Academicians Contact Form'}
+        self.assertEqual(EXPECTED, body['res'])
+        # self.assertIn("value", body['res']['schema'])
+        # self.assertIn("value", body['res']['schemaModifier'])
+        # self.assertNotIn("schema_versions", body['res'])
+        # self.assertNotIn("schemaModifier_versions", body['res'])
