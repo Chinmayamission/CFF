f76a8f46ded997d8e0d3584d9b954286d8ccec9f
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-06-30 09:48:20 -0400

    fix auth tests

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -1,5 +1,5 @@
 """
-python -m unittest tests.unit.test_auth
+pipenv run python -m unittest tests.unit.test_auth
 """
 import unittest
 from chalice.app import UnauthorizedError
@@ -9,6 +9,10 @@ import json
 from app import app
 import uuid
 import os
+from collections import namedtuple
+
+def dict_to_object(d):
+  return namedtuple('Struct', d.keys())(*d.values())
 
 COG_ID = "asd"
 class FormPermissions(unittest.TestCase):
@@ -21,13 +25,13 @@ class FormPermissions(unittest.TestCase):
       pass
     def test_check_permissions_owner(self):
       app.test_user_id = COG_ID
-      model = {"cff_permissions": {app.get_current_user_id(): {"owner": True}}}
+      model = dict_to_object({"cff_permissions": {app.get_current_user_id(): {"owner": True}}})
       for action in ("owner", "Responses_Edit", "Forms_Edit"):
         with self.subTest(action=action):
           self.assertTrue(app.check_permissions(model, action))
     def test_check_permissions_invalid(self):
       app.test_user_id = COG_ID
-      model = {"cff_permissions": {app.get_current_user_id(): {"Responses_View": True}}}
+      model = dict_to_object({"cff_permissions": {app.get_current_user_id(): {"Responses_View": True}}})
       for action in ("owner", "Responses_Edit", "Forms_Edit"):
         with self.subTest(action=action):
           with self.assertRaises(UnauthorizedError):
@@ -35,13 +39,13 @@ class FormPermissions(unittest.TestCase):
       self.assertTrue(app.check_permissions(model, "Responses_View"))
     def test_check_permissions_valid(self):
       app.test_user_id = COG_ID
-      model = {"cff_permissions": {app.get_current_user_id(): {"Responses_View": True, "Responses_CheckIn": True}}}
+      model = dict_to_object({"cff_permissions": {app.get_current_user_id(): {"Responses_View": True, "Responses_CheckIn": True}}})
       for action in ("Responses_View", ["Responses_View", "Responses_Edit"], ["Form_Edit", "Responses_CheckIn"]):
         with self.subTest(action=action):
           self.assertTrue(app.check_permissions(model, action))
     def test_check_permissions_invalid_false(self):
       app.test_user_id = COG_ID
-      model = {"cff_permissions": {app.get_current_user_id(): {"Responses_View": True, "Responses_Edit": False, "Forms_Edit": False, "owner": False}}}
+      model = dict_to_object({"cff_permissions": {app.get_current_user_id(): {"Responses_View": True, "Responses_Edit": False, "Forms_Edit": False, "owner": False}}})
       for action in ("owner", "Responses_Edit", "Forms_Edit"):
         with self.subTest(action=action):
           with self.assertRaises(UnauthorizedError):
