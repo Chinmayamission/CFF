6611fe1e175eaf021bd24b954584f0f259ff0b68
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-07-10 19:44:15 -0400

    Customize sign up page

--- scripts/src/common/Login/CustomSignUp.tsx
+++ scripts/src/common/Login/CustomSignUp.tsx
@@ -11,6 +11,7 @@ import {
     SectionBody,
     SectionFooter,
     InputRow,
+    FormRow,
     ButtonRow,
     Link
 } from 'aws-amplify-react/dist/AmplifyUI';
@@ -34,8 +35,11 @@ export default class CustomSignUp extends SignUp {
     }
 
     signUp() {
-        const { username, password, email, phone_number } = this.inputs;
-        Auth.signUp(username, password, email, phone_number)
+        const { email, password, name, center } = this.inputs;
+        const username = email;
+        Auth.signUp({username, password,
+                attributes: {"custom:center": center, name}
+            })
             .then(() => this.changeState('confirmSignUp', username))
             .catch(err => this.error(err));
     }
@@ -50,10 +54,18 @@ export default class CustomSignUp extends SignUp {
                 <SectionBody theme={theme}>
                     <InputRow
                         autoFocus
-                        placeholder={I18n.get('Username', 'Username')}
+                        placeholder={I18n.get('Name', 'Name')}
                         theme={theme}
-                        key="username"
-                        name="username"
+                        key="name"
+                        name="name"
+                        onChange={this.handleInputChange}
+                    />
+                    <InputRow
+                        placeholder={I18n.get('Email', 'Email')}
+                        theme={theme}
+                        type="email"
+                        key="email"
+                        name="email"
                         onChange={this.handleInputChange}
                     />
                     <InputRow
@@ -64,20 +76,13 @@ export default class CustomSignUp extends SignUp {
                         name="password"
                         onChange={this.handleInputChange}
                     />
-                    <InputRow
-                        placeholder={I18n.get('Email', 'Email')}
-                        theme={theme}
-                        key="email"
-                        name="email"
-                        onChange={this.handleInputChange}
-                    />
-                    <InputRow
-                        placeholder={I18n.get('Phone Number', 'Phone Number')}
-                        theme={theme}
-                        key="phone_number"
-                        name="phone_number"
-                        onChange={this.handleInputChange}
-                    />
+                    <FormRow>
+                        <select className="amplify-input form-control form-control-sm" name="center" onChange={this.handleInputChange}>
+                            <option value="" selected disabled>Select center</option>
+                            <option value="ccmt">CCMT</option>
+                            <option value="cmtc">CMTC</option>
+                        </select>
+                    </FormRow>
                     <ButtonRow onClick={this.signUp} theme={theme}>
                         {I18n.get('Sign Up', 'Sign Up')}
                     </ButtonRow>
@@ -90,7 +95,7 @@ export default class CustomSignUp extends SignUp {
                     </div>
                     <div style={Object.assign({textAlign: 'right'}, theme.col6)}>
                         <Link theme={theme} onClick={() => this.changeState('signIn')}>
-                            {I18n.get('Sign In', 'Confirm a Code')}
+                            {I18n.get('Sign In', 'Sign In')}
                         </Link>
                     </div>
                 </SectionFooter>
