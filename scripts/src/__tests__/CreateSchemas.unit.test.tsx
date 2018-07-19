7cc7d70d201a4e202a103a2a1ad6f008b636998d
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-07-19 18:14:08 -0400

    Change structure of specifiedShowFields

--- scripts/src/__tests__/CreateSchemas.unit.test.tsx
+++ scripts/src/__tests__/CreateSchemas.unit.test.tsx
@@ -2,16 +2,16 @@ import createSchemas from "src/common/CreateSchemas";
 
 it('create schemas normally', () => {
   let data = {
-    "schema": {"a": "b"},
-    "schemaModifier": {"a2": "b2"},
+    "schema": { "a": "b" },
+    "schemaModifier": { "a2": "b2" },
     "formOptions": {
-      "defaultFormData": {"a3": "b3"},
-      "paymentCalcInfo": {"a4": "b4"},
-      "dataOptions": {"a5": "b5"}
+      "defaultFormData": { "a3": "b3" },
+      "paymentCalcInfo": { "a4": "b4" },
+      "dataOptions": { "a5": "b5" }
     }
   };
   expect(createSchemas(data).schema).toEqual(data.schema);
-}); 
+});
 
 
 it('overrides schema with specifiedShowFields', () => {
@@ -19,22 +19,24 @@ it('overrides schema with specifiedShowFields', () => {
     "schema": {
       "title": "Hello",
       "description": "The description",
-      "parents": {
-        "minItems": 4
+      "properties": {
+        "parents": {
+          "minItems": 4
+        }
       }
     }
   };
   let specifiedShowFields = {
     "title": "New",
-    "parents": {
-      "minItems": 5
-    }
+    "properties.parents.minItems": 5
   };
   let expected_schema = {
     "title": "New",
     "description": "The description",
-    "parents": {
-      "minItems": 5
+    "properties": {
+      "parents": {
+        "minItems": 5
+      }
     }
   };
   expect(createSchemas(data, specifiedShowFields).schema).toEqual(expected_schema);
--- scripts/src/common/CreateSchemas.tsx
+++ scripts/src/common/CreateSchemas.tsx
@@ -1,9 +1,12 @@
-import {get, assign} from "lodash-es";
+import {get, set} from "lodash-es";
 export default function createSchemas(data, specifiedShowFields={}) {
+    for (let i in specifiedShowFields) {
+        set(data["schema"], i, specifiedShowFields[i]);
+    }
     return {
         schemaMetadata: get(data, "formOptions", {}),
         uiSchema: data["uiSchema"],
-        schema: assign(data["schema"], specifiedShowFields),
+        schema: data["schema"],
         defaultFormData: get(data, "formOptions.defaultFormData", {}),
         paymentCalcInfo: get(data, "formOptions.paymentInfo", {}),
         dataOptions: get(data, "formOptions.dataOptions", {}),
