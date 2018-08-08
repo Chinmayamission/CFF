a635a7c90f7ee6ff377fb7df5b4b325ea5196745
Ashwin Ramaswami <arama@Ashwins-MacBook-Pro.local>
2018-08-07 22:28:51 -0400

    Remove old checkin code

--- scripts/src/admin/ResponseTable/ResponseDetail.tsx
+++ scripts/src/admin/ResponseTable/ResponseDetail.tsx
@@ -44,89 +44,13 @@ class ResponseDetail extends React.Component<IResponseDetailProps, IResponseDeta
             });
         }
     }
-    changeCheckIn(row, checked) {
-        console.log(checked);
-        API.post("CFF", `responses/${this.props.responseId}/checkin`, {
-            "body":
-            {
-                "path": `value.${row.cff_accessor}.check_in`, // participants[0].check_in
-                "value": checked
-            }
-        }).then(e => {
-            console.log("Response update succeeded", e);
-            this.setState({ "data": e.res });
-        }).catch(e => {
-            alert(`Response update failed: ${e}`);
-        })
-    }
-    getColorFromRace(race) {
-        switch (race) {
-            case "Mela": return "#a4c2f4";
-            case "Half Marathon": return "#ff4e50";
-            case "5K": return "#00ff00";
-            case "10K": return "#fcfbe3"
-            default: return "#ffffff";
-        }
-    }
     render() {
-        let cell = row => row.value || "None";
-        let columns = [{
-            Header: "First Name",
-            accessor: "name.first",
-            Cell: cell
-        },
-        {
-            Header: "Last Name",
-            accessor: "name.last",
-            Cell: cell
-        },
-        {
-            Header: "T-shirt size",
-            accessor: "shirt_size",
-            Cell: cell
-        },
-        {
-            Header: "Race",
-            accessor: "race",
-            Cell: cell
-        },
-        {
-            Header: "Bib Number",
-            accessor: "bib_number",
-            Cell: cell
-        },
-        {
-            Header: "Check in",
-            accessor: "check_in",
-            Cell: ({ original }) => <input type="checkbox" checked={original.check_in} onChange={e => this.changeCheckIn(original, e.target.checked)} value="" id={`defaultCheck_${this.props.responseId}`} />
-        }
-        ];
-        let i = 0;
-        let showOmrunTable = get(this.props.dataOptions, "checkinTable.omrunCheckin") == true;
         if (!this.props.responseData) {
             return <div>Loading...</div>;
         }
         return (
             <div className="container-fluid cff-response-detail" key={this.props.responseId}>
                 <div className="row">
-                    {this.props.checkInMode && showOmrunTable && this.state.data.value.participants && <div className="card col-12 col-sm-6">
-                        <div className="card-body">
-                            <h5 className="card-title">Details</h5>
-                            <ReactTable
-                                data={this.state.data.value.participants.map(e => Object.assign({ "cff_accessor": `participants.${i++}` }, e))}
-                                columns={columns}
-                                minRows={0}
-                                showPagination={true}
-                                getTrProps={(state, rowInfo, column) => {
-                                    return {
-                                        style: {
-                                            backgroundColor: this.getColorFromRace(get(rowInfo.row, "race"))
-                                        }
-                                    }
-                                }}
-                            />
-                        </div>
-                    </div>}
                     <div className="card col-12 col-sm-6">
                         <div className="card-body">
                             <h5 className="card-title">Response Value</h5>
