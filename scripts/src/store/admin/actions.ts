703ee58b0ca786c01d1afea94296b6ece967fb45
Ashwin Ramaswami <arama@Ashwins-MacBook-Pro.local>
2018-08-13 09:54:09 +0800

    [Admin] Add loading animation to load form list

--- scripts/src/store/admin/actions.ts
+++ scripts/src/store/admin/actions.ts
@@ -1,11 +1,15 @@
 import { API } from "aws-amplify";
 import { IFormListItem } from "../../admin/FormList/FormList.d";
+import { loadingStart, loadingEnd } from "../base/actions";
 
 
 export const loadFormList = () => (dispatch, getState) => {
+  dispatch(loadingStart());
   return API.get("CFF", `forms`, {}).then(e => {
     dispatch(setFormList(e.res));
+    dispatch(loadingEnd());
   }).catch(e => {
+    dispatch(loadingEnd());
     console.error(e);
     alert("Error getting form list. " + e);
   });
