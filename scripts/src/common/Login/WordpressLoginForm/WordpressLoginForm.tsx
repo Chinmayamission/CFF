df38550f9573067d79c78a12ae93cd1a91489739
Ashwin Ramaswami <arama@Ashwins-MacBook-Pro.local>
2018-09-16 16:14:46 -0400

    [Auth] Go to proper wordpress auth page, styling

--- scripts/src/common/Login/WordpressLoginForm/WordpressLoginForm.tsx
+++ scripts/src/common/Login/WordpressLoginForm/WordpressLoginForm.tsx
@@ -3,22 +3,31 @@ import Login from "../Login";
 import "./WordpressLoginForm.scss";
 import { connect } from "react-redux";
 import { setBootstrap } from "../../../store/base/actions";
+import { setAuthPage } from "../../../store/auth/actions";
+import { IAuthState } from "../../../store/auth/types";
 
 
 const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    setBootstrap: e => dispatch(setBootstrap(e))
+    setBootstrap: e => dispatch(setBootstrap(e)),
+    setAuthPage: p => dispatch(setAuthPage(p))
 });
+interface WordpressLoginFormProps {
+    authPage: string,
+    setBootstrap: (e: boolean) => void,
+    setAuthPage: (e: string) => void
+}
 
-class WordpressLoginForm extends React.Component<any, any> {
+class WordpressLoginForm extends React.Component<WordpressLoginFormProps, any> {
     componentDidMount() {
+        this.props.setAuthPage(this.props.authPage);
         this.props.setBootstrap(false);
     }
     render() {
         return <div className="ccmt-cff-wordpress-login-form">
-            <Login />
+            <Login {...this.props} />
         </div>;
     }
 }
