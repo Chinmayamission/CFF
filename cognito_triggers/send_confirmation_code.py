83128ac80e1186b31f891e2ebafb60edb0267448
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-12-07 21:38:18 -0800

    Update lambda confirmation function to support ChinmayaEchoes code-based confirmation

--- cognito_triggers/send_confirmation_code.py
+++ cognito_triggers/send_confirmation_code.py
@@ -9,24 +9,37 @@ import os
 def lambda_handler(event, context):
   if event["userPoolId"] != os.environ["USER_POOL_ID"] or event["triggerSource"] != "CustomMessage_SignUp":
     return event
-  
-  code = event["request"]["codeParameter"]
-  username = event["request"]["userAttributes"]["sub"]
-  url = os.environ["API_ENDPOINT"]
-  link = f"{url}confirmSignUp?code={code}&username={username}"
-  
-  event["response"]["emailSubject"] = "Chinmaya Mission Account - Please verify your account";
-  event["response"]["smsMessage"] = f"Welcome to the service. Your confirmation code is {code}";
-  event["response"]["emailMessage"] = f"""
-  Hari OM,<br>
-  Please click on the link below to verify your email address.<br><br>
+  if event["callerContext"]["clientId"]==os.environ["CHINMAYA_ECHOES_CLIENT_ID"]:
+      code = event["request"]["codeParameter"]
+      username = event["request"]["userAttributes"]["sub"]
+      
+      event["response"]["emailSubject"] = "Chinmaya Mission Account - Please verify your account";
+      event["response"]["smsMessage"] = f"Welcome to the service. Your confirmation code is {code}";
+      event["response"]["emailMessage"] = f"""
+          Hari OM,<br>
+          Your verification code for ChinmayaEchoes is {code}<br>
+          Thanks,<br>
+          Chinmaya Mission IT Team
+          """
+      return event
+  else:
+      code = event["request"]["codeParameter"]
+      username = event["request"]["userAttributes"]["sub"]
+      url = os.environ["API_ENDPOINT"]
+      link = f"{url}confirmSignUp?code={code}&username={username}"
+      
+      event["response"]["emailSubject"] = "Chinmaya Mission Account - Please verify your account";
+      event["response"]["smsMessage"] = f"Welcome to the service. Your confirmation code is {code}";
+      event["response"]["emailMessage"] = f"""
+      Hari OM,<br>
+      Please click on the link below to verify your email address.<br><br>
 
-  <a href='{link}'>Verify your email address</a><br><br>
-  
-  If the link above does not work, please go to {link} in your browser.<br><br>
+      <a href='{link}'>Verify your email address</a><br><br>
+      
+      If the link above does not work, please go to {link} in your browser.<br><br>
 
-  Thanks,<br>
-  Chinmaya Mission IT Team
-  
-  """
-  return event
+      Thanks,<br>
+      Chinmaya Mission IT Team
+      
+      """
+      return event
