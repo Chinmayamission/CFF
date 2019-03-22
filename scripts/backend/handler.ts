6f9c945e33f939807354fcb76b8e69be79cc50f1
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-03-22 17:45:36 -0400

    feat: allow exporting of multiple google sheets

--- scripts/backend/handler.ts
+++ scripts/backend/handler.ts
@@ -132,191 +132,193 @@ module.exports.hello = async (event, context) => {
     }
 
     let forms = await coll.find({ '_cls': 'chalicelib.models.Form', 'formOptions.dataOptions.export': { '$exists': true } }).toArray();
-
     for (let form of forms) {
       const dataOptions = getOrDefaultDataOptions(form);
-      let googleSheetsDataOptionIndex = findIndex(dataOptions.export, { "type": "google_sheets" });
-      if (googleSheetsDataOptionIndex === -1) {
+      if (!dataOptions.export) {
         continue;
       }
-      let googleSheetsDataOption = dataOptions.export[googleSheetsDataOptionIndex];
+      for (let googleSheetsDataOptionIndex in dataOptions.export) {
+        let googleSheetsDataOption = dataOptions.export[googleSheetsDataOptionIndex];
+        if (googleSheetsDataOption.type !== "google_sheets") {
+          continue;
+        }
+        let filter = get(googleSheetsDataOption, "filter", {});
+        let responses = await coll.find({ '_cls': 'chalicelib.models.Response', form: form._id, ...filter }).sort({ date_created: -1 }).toArray();
+        let queryCache = {}; // keys: stringified versions of queries, values: list of responses
+        let extraHeaders = [];
 
-      let filter = get(googleSheetsDataOption, "filter", {});
-      let responses = await coll.find({ '_cls': 'chalicelib.models.Response', form: form._id, ...filter }).sort({ date_created: -1 }).toArray();
-      let queryCache = {}; // keys: stringified versions of queries, values: list of responses
-      let extraHeaders = [];
-
-      if (googleSheetsDataOption.enableOrderId) {
-        extraHeaders.push({
-          Header: "Order ID", accessor: e => {
-            let orderId = get(e, "admin_info.order_id", "");
-            return e.CFF_UNWIND_INDEX ? `${orderId}.${e.CFF_UNWIND_INDEX}` : `${orderId}`;
-          }
-        });
-        let maxId = get(maxBy(responses, e => get(e, "admin_info.order_id")), "admin_info.order_id", 0);
-        for (let i = responses.length - 1; i >= 0; i--) {
-          let response = responses[i];
-          if (!get(response, "admin_info.order_id")) {
-            maxId++;
-            await coll.updateOne({ _id: response._id }, { $set: { "admin_info.order_id": maxId } });
-            set(response, "admin_info.order_id", maxId);
+        if (googleSheetsDataOption.enableOrderId) {
+          extraHeaders.push({
+            Header: "Order ID", accessor: e => {
+              let orderId = get(e, "admin_info.order_id", "");
+              return e.CFF_UNWIND_INDEX ? `${orderId}.${e.CFF_UNWIND_INDEX}` : `${orderId}`;
+            }
+          });
+          let maxId = get(maxBy(responses, e => get(e, "admin_info.order_id")), "admin_info.order_id", 0);
+          for (let i = responses.length - 1; i >= 0; i--) {
+            let response = responses[i];
+            if (!get(response, "admin_info.order_id")) {
+              maxId++;
+              await coll.updateOne({ _id: response._id }, { $set: { "admin_info.order_id": maxId } });
+              set(response, "admin_info.order_id", maxId);
+            }
           }
         }
-      }
-      if (googleSheetsDataOption.enableNearestLocation) {
-        const locations = googleSheetsDataOption.nearestLocationOptions.locations;
-        extraHeaders.push({
-          Header: "Nearest Location", accessor: e => get(e, "admin_info.nearest_location", "")
-        });
-        // const addressAccessor = googleSheetsDataOption.nearestLocationOptions.addressAccessor;
-        let responsesToCalculate = responses.filter(response => !get(response, "admin_info.nearest_location"));
+        if (googleSheetsDataOption.enableNearestLocation) {
+          const locations = googleSheetsDataOption.nearestLocationOptions.locations;
+          extraHeaders.push({
+            Header: "Nearest Location", accessor: e => get(e, "admin_info.nearest_location", "")
+          });
+          // const addressAccessor = googleSheetsDataOption.nearestLocationOptions.addressAccessor;
+          let responsesToCalculate = responses.filter(response => !get(response, "admin_info.nearest_location"));
 
-        // Todo: Google limits to 100 elements -- find a better way to do it in batch rather than each one individually https://developers.google.com/maps/documentation/javascript/distancematrix#usage_limits_and_requirements
-        let distanceRows: IDistanceMatrixRow[] = [];
-        for (let response of responsesToCalculate) {
-          let results = (await googleMapsClient.distanceMatrix({
-            origins: [`${response.value.address.line1} ${response.value.address.city} ${response.value.address.state} ${response.value.address.zipcode}`],
-            destinations: locations.map(e => [e.latitude, e.longitude])
-          }).asPromise());
-          distanceRows.push(results.json.rows[0]);
+          // Todo: Google limits to 100 elements -- find a better way to do it in batch rather than each one individually https://developers.google.com/maps/documentation/javascript/distancematrix#usage_limits_and_requirements
+          let distanceRows: IDistanceMatrixRow[] = [];
+          for (let response of responsesToCalculate) {
+            let results = (await googleMapsClient.distanceMatrix({
+              origins: [`${response.value.address.line1} ${response.value.address.city} ${response.value.address.state} ${response.value.address.zipcode}`],
+              destinations: locations.map(e => [e.latitude, e.longitude])
+            }).asPromise());
+            distanceRows.push(results.json.rows[0]);
+          }
+
+          for (const i in responsesToCalculate) {
+            const distanceRow: IDistanceMatrixRow = distanceRows[i];
+            let response = responsesToCalculate[i];
+            const nearestLocation = minBy(distanceRow.elements, e => get(e, "duration.value", Number.MAX_SAFE_INTEGER));
+            if (nearestLocation.status !== "OK") { // Status is OK or NOT_FOUND.
+              continue;
+            }
+            const nearestLocationIndex = distanceRow.elements.indexOf(nearestLocation);
+            const nearestLocationName = locations[nearestLocationIndex].name;
+            console.log(nearestLocationName);
+            set(response, "admin_info.nearest_location", nearestLocationName);
+            await coll.updateOne({ _id: response._id }, { $set: { "admin_info.nearest_location": nearestLocationName } });
+          }
         }
 
-        for (const i in responsesToCalculate) {
-          const distanceRow: IDistanceMatrixRow = distanceRows[i];
-          let response = responsesToCalculate[i];
-          const nearestLocation = minBy(distanceRow.elements, e => get(e, "duration.value", Number.MAX_SAFE_INTEGER));
-          if (nearestLocation.status !== "OK") { // Status is OK or NOT_FOUND.
+        let spreadsheetId = googleSheetsDataOption.spreadsheetId;
+        if (!spreadsheetId) {
+          spreadsheetId = await createSpreadsheet(`CFF Export - ${form.name}`);
+          await coll.updateOne({ _id: form._id }, { "$set": { [`formOptions.dataOptions.export.${googleSheetsDataOptionIndex}.spreadsheetId`]: spreadsheetId } });
+        }
+        const spreadsheet = await promisify(sheets.spreadsheets.get)({ spreadsheetId });
+        const existingSheets: ISheet[] = spreadsheet.data.sheets;
+
+        let requests = [];
+        let viewsToShow = googleSheetsDataOption.viewsToShow || dataOptions.views.map(e => e.id);
+        let sheetsToDelete: ISheet[] = existingSheets.filter(e => viewsToShow.indexOf(e.properties.title) === -1);
+        for (const i in dataOptions.views) {
+          const dataOptionView = dataOptions.views[i];
+          if (viewsToShow.indexOf(dataOptionView.id) === -1) {
             continue;
           }
-          const nearestLocationIndex = distanceRow.elements.indexOf(nearestLocation);
-          const nearestLocationName = locations[nearestLocationIndex].name;
-          console.log(nearestLocationName);
-          set(response, "admin_info.nearest_location", nearestLocationName);
-          await coll.updateOne({ _id: response._id }, { $set: { "admin_info.nearest_location": nearestLocationName } });
-        }
-      }
-
-      let spreadsheetId = googleSheetsDataOption.spreadsheetId;
-      if (!spreadsheetId) {
-        spreadsheetId = await createSpreadsheet(`CFF Export - ${form.name}`);
-        await coll.updateOne({ _id: form._id }, { "$set": { [`formOptions.dataOptions.export.${googleSheetsDataOptionIndex}.spreadsheetId`]: spreadsheetId } });
-      }
-      const spreadsheet = await promisify(sheets.spreadsheets.get)({ spreadsheetId });
-      const existingSheets: ISheet[] = spreadsheet.data.sheets;
-
-      let requests = [];
-      let viewsToShow = googleSheetsDataOption.viewsToShow || dataOptions.views.map(e => e.id);
-      let sheetsToDelete: ISheet[] = existingSheets.filter(e => viewsToShow.indexOf(e.properties.title) === -1);
-      for (const i in dataOptions.views) {
-        const dataOptionView = dataOptions.views[i];
-        if (viewsToShow.indexOf(dataOptionView.id) === -1) {
-          continue;
-        }
-        let sheetId = Math.floor(stringHash(dataOptionView.id) / 10);
-        let title = dataOptionView.id;
-        let responsesToUse = responses;
-        if (dataOptionView.aggregate) {
-          let cacheKey = JSON.stringify(dataOptionView.aggregate);
-          let aggregateQuery = dataOptionView.aggregate.map(obj => renameKeys(obj, key => key.replace(/\|\|/g, ".").replace(/\|/g, "$") ));
-          if (queryCache[cacheKey]) {
-            responsesToUse = queryCache[cacheKey];
-          } else {
-            responsesToUse = await coll.aggregate([
-              {'$match': { '_cls': 'chalicelib.models.Response', form: form._id}},
-              ...aggregateQuery
-            ]).toArray();
-            queryCache[cacheKey] = responsesToUse;
+          let sheetId = Math.floor(stringHash(dataOptionView.id) / 10);
+          let title = dataOptionView.id;
+          let responsesToUse = responses;
+          if (dataOptionView.aggregate) {
+            let cacheKey = JSON.stringify(dataOptionView.aggregate);
+            let aggregateQuery = dataOptionView.aggregate.map(obj => renameKeys(obj, key => key.replace(/\|\|/g, ".").replace(/\|/g, "$") ));
+            if (queryCache[cacheKey]) {
+              responsesToUse = queryCache[cacheKey];
+            } else {
+              responsesToUse = await coll.aggregate([
+                {'$match': { '_cls': 'chalicelib.models.Response', form: form._id}},
+                ...aggregateQuery
+              ]).toArray();
+              queryCache[cacheKey] = responsesToUse;
+            }
+            // Debug
+            // console.log(JSON.stringify(aggregateQuery), responsesToUse);
           }
-          // Debug
-          // console.log(JSON.stringify(aggregateQuery), responsesToUse);
-        }
-        if (responsesToUse.length === 0) {
-          continue;
-        }
-        let { headers, dataFinal } = createHeadersAndDataFromDataOption(responsesToUse, form, dataOptionView, null, dataOptionView.aggregate ? true: false);
-        if (!dataOptionView.aggregate) {
-          headers = [...extraHeaders, ...headers];
-        }
-        let rowCount = dataFinal.length + 1;
-        let columnCount = headers.length;
-        let existingSheet = find(existingSheets, e => e.properties.title === title);
-        if (!existingSheet) {
+          if (responsesToUse.length === 0) {
+            continue;
+          }
+          let { headers, dataFinal } = createHeadersAndDataFromDataOption(responsesToUse, form, dataOptionView, null, dataOptionView.aggregate ? true: false);
+          if (!dataOptionView.aggregate) {
+            headers = [...extraHeaders, ...headers];
+          }
+          let rowCount = dataFinal.length + 1;
+          let columnCount = headers.length;
+          let existingSheet = find(existingSheets, e => e.properties.title === title);
+          if (!existingSheet) {
+            requests.push({
+              addSheet: {
+                properties: {
+                  sheetId,
+                  title
+                }
+              }
+            });
+          }
+          else {
+            sheetId = existingSheet.properties.sheetId;
+          }
           requests.push({
-            addSheet: {
+            updateSheetProperties: {
+              fields: 'gridProperties.rowCount, gridProperties.columnCount',
               properties: {
                 sheetId,
-                title
+                gridProperties: {
+                  rowCount,
+                  columnCount
+                }
+              }
+            }
+          });
+          requests.push({
+            updateCells: {
+              rows: [
+                {
+                  values: headers.map(header => ({
+                    userEnteredValue: { stringValue: header.Header }
+                  }))
+                },
+                ...dataFinal.map(response => ({
+                  values: headers.map(header => ({
+                    userEnteredValue: { stringValue: Headers.formatValue(header.accessor(response)) }
+                  }))
+                }))
+              ],
+              fields: '*',
+              start: {
+                sheetId,
+                rowIndex: 0,
+                columnIndex: 0
+              }
+            }
+          });
+          requests.push({
+            autoResizeDimensions: {
+              dimensions: {
+                sheetId,
+                dimension: "COLUMNS",
+                startIndex: 0,
+                endIndex: headers.length
               }
             }
           });
         }
-        else {
-          sheetId = existingSheet.properties.sheetId;
+        for (let sheetToDelete of sheetsToDelete) {
+          requests.push({
+            deleteSheet: {
+              sheetId: sheetToDelete.properties.sheetId
+            }
+          });
         }
-        requests.push({
-          updateSheetProperties: {
-            fields: 'gridProperties.rowCount, gridProperties.columnCount',
-            properties: {
-              sheetId,
-              gridProperties: {
-                rowCount,
-                columnCount
-              }
-            }
-          }
-        });
-        requests.push({
-          updateCells: {
-            rows: [
-              {
-                values: headers.map(header => ({
-                  userEnteredValue: { stringValue: header.Header }
-                }))
-              },
-              ...dataFinal.map(response => ({
-                values: headers.map(header => ({
-                  userEnteredValue: { stringValue: Headers.formatValue(header.accessor(response)) }
-                }))
-              }))
-            ],
-            fields: '*',
-            start: {
-              sheetId,
-              rowIndex: 0,
-              columnIndex: 0
-            }
-          }
-        });
-        requests.push({
-          autoResizeDimensions: {
-            dimensions: {
-              sheetId,
-              dimension: "COLUMNS",
-              startIndex: 0,
-              endIndex: headers.length
-            }
-          }
-        });
-      }
-      for (let sheetToDelete of sheetsToDelete) {
-        requests.push({
-          deleteSheet: {
-            sheetId: sheetToDelete.properties.sheetId
-          }
-        });
-      }
-      if (requests.length === 0) {
-        continue;
-      }
-      let response = await promisify(sheets.spreadsheets.batchUpdate)({
-        spreadsheetId,
-        resource: {
-          requests
+        if (requests.length === 0) {
+          continue;
         }
-      });
-      if (response.status !== 200) {
-        // todo: error handling/logging here.
-        throw response;
+        let response = await promisify(sheets.spreadsheets.batchUpdate)({
+          spreadsheetId,
+          resource: {
+            requests
+          }
+        });
+        if (response.status !== 200) {
+          // todo: error handling/logging here.
+          throw response;
+        }
       }
     }
 
