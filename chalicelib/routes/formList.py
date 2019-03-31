429cee04c24df3558fd1e6285f13a21123a8d952
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-03-31 16:17:47 -0700

    feat: return date_created and date_modified from forms list

--- chalicelib/routes/formList.py
+++ chalicelib/routes/formList.py
@@ -1,9 +1,11 @@
 from boto3.dynamodb.conditions import Key
 from chalicelib.models import Form, serialize_model
+import random
 
 def form_list():
     """Get forms user has access to."""
     from ..main import app, TABLES
     userId = app.get_current_user_id()
-    forms = Form.objects.raw({f"cff_permissions.{userId}": {"$exists": True}}).only("name", "cff_permissions")
-    return {"res": serialize_model(list(forms))}
+    forms = Form.objects.raw({f"cff_permissions.{userId}": {"$exists": True}}).only("name", "cff_permissions", "date_modified", "date_created")
+    forms = serialize_model(list(forms))
+    return {"res": forms}
