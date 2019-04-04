7930930bffb9c501d986cabd7b0687538e623752
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-04-04 13:55:55 -0700

    fix: add another MakeHeaders test

--- scripts/src/__tests__/admin/MakeHeaders.unit.test.tsx
+++ scripts/src/__tests__/admin/MakeHeaders.unit.test.tsx
@@ -56,6 +56,7 @@ it('renders custom columns for main view in response table', () => {
     };
     const result = Headers.makeHeadersFromDataOption(dataOptionView, schema);
     expect(result).toMatchSnapshot();
+    expect(result[0].Cell({"parents": {"name": {"first": "a", "last": "b"}}})).toEqual("a");
 });
 
 it('renders custom columns for unwind by view in response table', () => {
@@ -88,4 +89,35 @@ it('renders custom columns for unwind by view in response table', () => {
     };
     const result = Headers.makeHeadersFromDataOption(dataOptionView, schema);
     expect(result).toMatchSnapshot();
+});
+
+it('renders custom columns with constant values for main view in response table', () => {
+    const dataOptionView: IDataOptionView = {
+        "id": "main",
+        "displayName": "Main",
+        "columns": [
+            { "label": "Names", "value": ["parents.name.last", "parents.name.first"] }
+        ]
+    };
+    const schema = {
+        "type": "object",
+        "properties": {
+            "name": {
+                "type": "object",
+                "properties": {
+                    "first": { "type": "string" },
+                    "last": { "type": "string" }
+                }
+            },
+            "parents": {
+                "type": "object",
+                "properties": {
+                    "age": { "type": "number" }
+                }
+            }
+        }
+    };
+    const result = Headers.makeHeadersFromDataOption(dataOptionView, schema);
+    expect(result).toMatchSnapshot();
+    expect(result[0].Cell({"parents": {"name": {"first": "a", "last": "b"}}})).toEqual("a b");
 });
