f2b0281d0864ced9754e07d576e47d07f18cee18
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-04-17 21:29:48 -0700

    feat: allow form editing with "query" query string functionality

--- chalicelib/routes/formResponseList.py
+++ chalicelib/routes/formResponseList.py
@@ -2,12 +2,28 @@ from ..util import get_all_responses
 from boto3.dynamodb.conditions import Key
 from chalicelib.models import Form, Response, serialize_model
 from bson.objectid import ObjectId
+from pydash.objects import get
 
 def form_response_list(formId):
     """Show all responses for a particular form."""
     from ..main import app
     form = Form.objects.only("formOptions", "cff_permissions").get({"_id":ObjectId(formId)})
-    app.check_permissions(form, ["Responses_View", "Responses_CheckIn"])
     # todo: use search framework, don't return all!
-    responses = Response.objects.raw({"form": form.id})
+    query = app.current_request.query_params and app.current_request.query_params.get("query", None)
+    if query:
+        app.check_permissions(form, ["Responses_View", "Responses_Checkin"])
+        search_fields = get(form.formOptions, "search.searchFields", ["_id"])
+        result_limit = get(form.formOptions, "search.resultLimit", 10)
+        result_fields = get(form.formOptions, "search.resultFields", ["_id"])
+        mongo_query = {}
+        for field in search_fields:
+            mongo_query[field] = {"$regex": "^" + query}
+        mongo_query["form"] = form.id
+        projection = {}
+        for field in result_fields:
+            projection[field] = 1
+        responses = Response.objects.raw({"form": form.id}).limit(result_limit).project(projection)
+    else:
+        app.check_permissions(form, ["Responses_View"])
+        responses = Response.objects.raw({"form": form.id})
     return {"res": [serialize_model(r) for r in responses]}
--- tests/integration/test_formResponses.py
+++ tests/integration/test_formResponses.py
@@ -10,6 +10,7 @@ from app import app
 from tests.integration.baseTestCase import BaseTestCase
 from chalicelib.models import Response, Form
 from bson.objectid import ObjectId
+from unittest.mock import MagicMock
 
 class FormResponses(BaseTestCase):
     def setUp(self):
@@ -25,6 +26,14 @@ class FormResponses(BaseTestCase):
         self.assertEqual(response['statusCode'], 200, response)
         body = json.loads(response['body'])
         self.assertTrue(len(body['res']) > 0, "Response list is empty.")
+    def test_form_responses_search(self):
+        """Search the response list."""
+        response = self.lg.handle_request(method='GET', headers={"authorization": "auth",}, body='',
+                                          path=f'/forms/{self.formId}/responses/?query=test')
+        self.assertEqual(response['statusCode'], 200, response)
+        body = json.loads(response['body'])
+        self.assertTrue(len(body['res']) > 0, "Response list is empty.")
+        self.assertEqual(list(body['res'][0].keys()), ['_id'])
     def test_response_summary(self):
         """View aggregate summary of data."""
         response = self.lg.handle_request(method='GET', headers={"authorization": "auth",}, body='',
