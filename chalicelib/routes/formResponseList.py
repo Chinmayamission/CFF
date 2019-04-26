d0720177bf64cc998aff5586ec36cb401096e099
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-04-26 09:03:20 -0700

    fix: allow searching in an array

--- chalicelib/routes/formResponseList.py
+++ chalicelib/routes/formResponseList.py
@@ -6,13 +6,18 @@ from bson.objectid import ObjectId
 from pydash.objects import get
 
 def form_response_list(formId):
-    """Show all responses for a particular form."""
+    """Show all responses for a particular form.
+    Example
+    /responses?query=test
+    /responses?query=test&autocomplete=1
+    """
     from ..main import app
     form = Form.objects.only("formOptions", "cff_permissions").get({"_id":ObjectId(formId)})
     # todo: use search framework, don't return all!
     query = app.current_request.query_params and app.current_request.query_params.get("query", None)
-    autocomplete = app.current_request.query_params.get("autocomplete", None)
+    autocomplete = app.current_request.query_params and app.current_request.query_params.get("autocomplete", None)
     if query:
+        # autocomplete, participant name, assign bibs functionality
         app.check_permissions(form, ["Responses_View", "Responses_Checkin"])
         search_fields = get(form.formOptions.dataOptions, "search.searchFields", ["_id"])
         result_limit = get(form.formOptions.dataOptions, "search.resultLimit", 10)
@@ -29,7 +34,18 @@ def form_response_list(formId):
                     except bson.errors.InvalidId:
                         pass
             else:
-                mongo_query["$or"].append({field: {"$regex": "^" + query, "$options" : "i"}})
+                if field.startswith("value.participants."):
+                    _, subfield = field.split("value.participants.")
+                    mongo_query["$or"].append({"value.participants": {
+                            "$elemMatch": {
+                                subfield: {
+                                    "$regex": '^' + query, "$options" : "i"
+                                }
+                            }
+                        }
+                    })
+                else:
+                    mongo_query["$or"].append({field: {"$regex": '^' + query, "$options" : "i"}})
         mongo_query["form"] = form.id
         if len(mongo_query["$or"]) == 0:
             del mongo_query["$or"]
