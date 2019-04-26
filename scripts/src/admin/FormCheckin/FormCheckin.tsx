f53ee17a9e23147137ec923c2b216562e1a27bcf
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-04-25 21:06:43 -0700

    fix: add form checkin to right spot

--- scripts/src/admin/FormCheckin/FormCheckin.tsx
+++ scripts/src/admin/FormCheckin/FormCheckin.tsx
@@ -34,72 +34,39 @@ class FormCheckin extends React.Component<IFormCheckinProps, IFormCheckinState> 
         }
     }
 
-    renderCard(props) {
-         return (<div className="card" style={{ width: '100%' }}>
+    render() {
+        const Card = ({ response }) => <div className="card" style={{ width: '100%' }}>
             <div className="card-body">
-                <h5 className="card-title">{props.response.value.contact_name.first} {props.response.value.contact_name.last}</h5>
+                <h5 className="card-title">{response.value.contact_name.first} {response.value.contact_name.last}</h5>
                 <div className="card-text">
-                    <div>{props.response._id.$oid.substring(0, 4)}</div>
-                    <div>{props.response.value.email}</div>
+                    <div>{response._id.$oid.substring(0, 4)}</div>
+                    <div>{response.value.email}</div>
                     <table className="table table-sm table-responsive">
                         <tbody>
-                            {props.response.value.participants.map((participant, i) => <tr style={raceRowStyle[participant.race]}>
+                            {response.value.participants.map((participant, i) => <tr style={raceRowStyle[participant.race]}>
                                 <td>{participant.name.last}</td>
                                 <td>{participant.name.first}</td>
                                 <td>{participant.shirt_size}</td>
-                                {/* <td>{participant.age}</td> */}
-                                {/* <td>{participant.gender}</td> */}
                                 <td>{participant.race}</td>
                                 <td>{participant.bib_number}</td>
                                 <td>
-                                        <input type="checkbox"
-                                            checked={participant.checkin}
-                                            onChange={e => props.editResponse(props.response._id.$oid, `participants.${i}.checkin`, e.target.checked)} />
-                                </td> 
+                                    <input type="checkbox"
+                                        checked={participant.checkin}
+                                        onChange={e => this.props.editResponse(response._id.$oid, `participants.${i}.checkin`, e.target.checked)} />
+                                </td>
                             </tr>)}
                         </tbody>
-                        
                     </table>
                 </div>
-                <button onClick={()=> {
-                     props.response.value.participants.map((participant, i) =>
-                        props.editResponse(props.response._id.$oid, `participants.${i}.checkin`, true)
-                )
-                          
+                <button className="btn btn-sm" onClick={() => {
+                    response.value.participants.map((participant, i) =>
+                        this.props.editResponse(response._id.$oid, `participants.${i}.checkin`, true)
+                    )
+
                 }}>Check in all</button>
             </div>
-        </div>)
+        </div>;
 
-    }
-
-    render() {
-        const Card = ({response}) => <div className="card" style={{ width: '100%' }}>
-                     <div className="card-body">
-                       <h5 className="card-title">{response.value.contact_name.first} {response.value.contact_name.last}</h5>
-                       <div className="card-text">
-                           <div>{response._id.$oid.substring(0, 4)}</div>
-                           <div>{response.value.email}</div>
-                           <table className="table table-sm table-responsive">
-                               <tbody>
-                                   {response.value.participants.map((participant, i) => <tr style={raceRowStyle[participant.race]}>
-                                       <td>{participant.name.last}</td>
-                                       <td>{participant.name.first}</td>
-                                       <td>{participant.shirt_size}</td>
-                                       <td>{participant.race}</td>
-                                       <td>{participant.bib_number}</td>
-                                       <td>
-                                           <input type="checkbox"
-                                               checked={participant.checkin}
-                                               onChange={e => this.props.editResponse(response._id.$oid, `participants.${i}.checkin`, e.target.checked)} />
-                                       </td>
-                                   </tr>)}
-                               </tbody>
-                           </table>
-                       </div>
-                       {/* <a className="btn btn-sm" onChange={e => this.checkInAll(response._id.$oid)}>Check in all</a> */}
-                   </div>
-               </div>;
-        
         return (<div>
             <form onSubmit={e => { e.preventDefault(); this.search(); return false; }}>
                 <div className=" d-none d-sm-block">
