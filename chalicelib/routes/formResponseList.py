085a5862ba048372fd49ef8599a62c6a10ecb169
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-04-26 06:51:58 -0700

    feat: autocomplete feature for search

--- chalicelib/routes/formResponseList.py
+++ chalicelib/routes/formResponseList.py
@@ -11,11 +11,13 @@ def form_response_list(formId):
     form = Form.objects.only("formOptions", "cff_permissions").get({"_id":ObjectId(formId)})
     # todo: use search framework, don't return all!
     query = app.current_request.query_params and app.current_request.query_params.get("query", None)
+    autocomplete = app.current_request.query_params.get("autocomplete", None)
     if query:
         app.check_permissions(form, ["Responses_View", "Responses_Checkin"])
         search_fields = get(form.formOptions.dataOptions, "search.searchFields", ["_id"])
         result_limit = get(form.formOptions.dataOptions, "search.resultLimit", 10)
         result_fields = get(form.formOptions.dataOptions, "search.resultFields", ["_id"])
+        autocomplete_fields = get(form.formOptions.dataOptions, "search.autocompleteFields", ["_id"])
         mongo_query = {"$or": []}
         for field in search_fields:
             if field == "_id":
@@ -36,10 +38,12 @@ def form_response_list(formId):
             mongo_query = {"$and": [{"paid": True}, mongo_query]}
         else:
             {"paid": True}
-        projection = {}
-        for field in result_fields:
-            projection[field] = 1
-        print(mongo_query)
+        if autocomplete is not None:
+            projection = {field: 1 for field in autocomplete_fields}
+        else:
+            projection = {}
+            for field in result_fields:
+                projection[field] = 1
         responses = Response.objects.raw(mongo_query).limit(result_limit).project(projection)
     else:
         app.check_permissions(form, ["Responses_View"])
