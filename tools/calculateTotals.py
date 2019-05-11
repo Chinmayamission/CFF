8943ab152f5cde51998d6ce0d924b2f03b0a7bee
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-11 08:36:17 -0700

    calculate paypal total script

---
+++ tools/calculateTotals.py
@@ -0,0 +1,44 @@
+"""
+pipenv run python -m unittest tools.calculateTotals
+Calculate totals.
+"""
+import os
+import dateutil.parser
+import boto3
+import json
+import datetime
+from bson.objectid import ObjectId
+from boto3.dynamodb.conditions import Key, Attr
+from decimal import Decimal
+from pydash.arrays import find_index
+
+PROD = True
+
+formId = "5c6b9b76780b47000197418f" if PROD else "...."
+
+os.environ["AWS_PROFILE"] = "ashwin-cff-lambda"
+os.environ["MODE"] = "BETA"
+os.environ["DB_NAME"] = "cff_prod" if PROD else "cff_beta"
+os.environ["USER_POOL_ID"] = ""
+os.environ["S3_UPLOADS_BUCKET_NAME"] = ""
+os.environ["COGNITO_CLIENT_ID"] = ""
+
+from chalicelib.main import app, MODE
+print("MODE", MODE)
+from chalicelib.models import Response, Form, PaymentTrailItem, PaymentStatusDetailItem, serialize_model
+
+# sanity check -- no one is marked as "not paid" with a zero total.
+# responses = Response.objects.raw({"form": ObjectId(formId), "paid": False, "paymentInfo.total": 0})
+# responses = Response.objects.raw({"form": ObjectId(formId), "paid": False, "amount_paid": {"$ne": "0"} })
+# print(list(responses))
+
+responses = Response.objects.raw({"form": ObjectId(formId)}).aggregate(
+    {"$unwind": "payment_status_detail"},
+    {"$match": {"payment_status_detail.method": "paypal_ipn"}},
+    {"$project": { "payment_status_detail.amount": 1 } }
+)
+
+total = sum(float(item["payment_status_detail"]["amount"]) for item in responses)
+
+print(total)
+#.objects.raw({"form": ObjectId(formId), "paid": True})
