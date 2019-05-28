59d90ab094e6b71d34697fb9f90e142bfbfdb6b3
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-27 18:53:58 -0700

    doc: move sameas documentation

--- scripts/src/form/form_widgets/SameAsField.tsx
+++ scripts/src/form/form_widgets/SameAsField.tsx
@@ -3,27 +3,6 @@ import ObjectField from "react-jsonschema-form/lib/components/fields/ObjectField
 import {get} from "lodash";
 
 
-/*
-DESCRIPTION:
-This will render a checkbox saying something like, "same as first participant's name".
-
-USAGE: 
-in the ui schema:
-{
-    "ui:field": "cff:sameAs",
-    "ui:options": {
-      "cff:sameAsFieldPath": "contact_name",
-      "cff:sameAsFieldName": "contact name"
-    }
-}
-
-If you want an array to show sameAs only for the first one, add the following to the uiSchema in the array:
-
-{
-    "classNames": "ccmt-cff-array-sameAs-showFirst"
-}
-
-*/
 class SameAsField extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
