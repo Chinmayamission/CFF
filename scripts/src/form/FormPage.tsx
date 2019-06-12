0fd4eea37eac15905bd8b896660e4f2506762d4f
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-06-12 06:51:32 -0700

    fix: fix form submissions closed blank screen issue

--- scripts/src/form/FormPage.tsx
+++ scripts/src/form/FormPage.tsx
@@ -262,13 +262,13 @@ class FormPage extends React.Component<IFormPageProps, IFormPageState> {
     if (get(this.state.formOptions, "responseSubmissionEnabled", true) === false) {
       return (<div>
         <h1>Submissions Closed</h1>
-        <p>Submissions are closed for the form: {htmlToText(this.state.schema.title)}</p>
+        <p>Submissions are closed for the form: {htmlToText.fromString(get(this.state.schema, "title", "CFF Form"), {"ignoreImage": true, "ignoreHref": true})}</p>
       </div>)
     }
     if (get(this.state.formOptions, "responseModificationEnabled", true) === false && this.state.status !== STATUS_FORM_CONFIRMATION && this.state.status !== STATUS_FORM_RESPONSE_VIEW && this.state.responseId) {
       return (<div>
         <h1>Response Modifications Closed</h1>
-        <p>Response modifications are closed for the form: {htmlToText(this.state.schema.title)}</p>
+        <p>Response modifications are closed for the form: {htmlToText.fromString(get(this.state.schema, "title", "CFF Form"), {"ignoreImage": true, "ignoreHref": true})}</p>
       </div>)
     }
     let formToReturn = (
