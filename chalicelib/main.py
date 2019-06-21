bddb2aa2328a7160b4ad86d004a96a38a9d2f5c6
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-06-21 17:22:14 +0000

    fix: remove create_app function to fix server

--- chalicelib/main.py
+++ chalicelib/main.py
@@ -8,6 +8,19 @@ import uuid
 import logging
 from pymodm.errors import DoesNotExist
 
+import pymodm
+from chalicelib.util.jwt import get_claims
+from chalicelib.models import User
+from chalicelib import routes
+
+MODE = os.getenv("MODE", "DEV")
+print("MODE IS " + MODE)
+USER_POOL_ID = os.getenv("USER_POOL_ID")
+COGNITO_CLIENT_ID = os.getenv("COGNITO_CLIENT_ID")
+S3_UPLOADS_BUCKET_NAME = os.getenv("S3_UPLOADS_BUCKET_NAME")
+# app = None
+PROD = True if MODE == "PROD" else False
+
 class CustomChalice(Chalice):
     test_user_id = None
     def get_url(self, path=''):
@@ -48,148 +61,108 @@ class CustomChalice(Chalice):
         else:
             raise UnauthorizedError("User {} is not authorized to perform action {} on this resource.".format(id, actions))
 
-def create_app(MODE):
-    import pymodm
-    from chalicelib.util.jwt import get_claims
-    from chalicelib.models import User
-    from chalicelib import routes
-    ssm = boto3.client('ssm', 'us-east-1')
-    s3_client = boto3.client('s3', "us-east-1")
-    if MODE == "TEST":
-        pymodm.connection.connect("mongodb://localhost:10255/admin")
-    elif MODE == "DEV":
-        pymodm.connection.connect("mongodb://localhost:10255/admin")
-    elif MODE == "BETA":
-        mongo_conn_str = ssm.get_parameter(Name='CFF_COSMOS_CONN_STR_WRITE_BETA', WithDecryption=True)['Parameter']['Value']
-        pymodm.connection.connect(mongo_conn_str)
-    elif MODE == "PROD":
-        mongo_conn_str = ssm.get_parameter(Name='CFF_COSMOS_CONN_STR_WRITE_PROD', WithDecryption=True)['Parameter']['Value']
-        pymodm.connection.connect(mongo_conn_str)
 
-    app = CustomChalice(app_name='ccmt-cff-rest-api')
-    if MODE != "PROD":
-        app.debug = True
-        app.log.setLevel(logging.DEBUG)
+ssm = boto3.client('ssm', 'us-east-1')
+s3_client = boto3.client('s3', "us-east-1")
+if MODE == "TEST":
+    pymodm.connection.connect("mongodb://localhost:10255/admin")
+elif MODE == "DEV":
+    pymodm.connection.connect("mongodb://localhost:10255/admin")
+elif MODE == "BETA":
+    mongo_conn_str = ssm.get_parameter(Name='CFF_COSMOS_CONN_STR_WRITE_BETA', WithDecryption=True)['Parameter']['Value']
+    pymodm.connection.connect(mongo_conn_str)
+elif MODE == "PROD":
+    mongo_conn_str = ssm.get_parameter(Name='CFF_COSMOS_CONN_STR_WRITE_PROD', WithDecryption=True)['Parameter']['Value']
+    pymodm.connection.connect(mongo_conn_str)
 
-    # This hack allows for integration testing.
-    test_user_id = os.getenv("DEV_COGNITO_IDENTITY_ID")
-    if test_user_id:
-        app.test_user_id = test_user_id
+app = CustomChalice(app_name='ccmt-cff-rest-api')
+if MODE != "PROD":
+    app.debug = True
+    app.log.setLevel(logging.DEBUG)
 
-    @app.authorizer()
-    def iamAuthorizer(auth_request):
-        """
-        {'sub': 'f31c1cb8-681c-4d3e-9749-d7c074ffd7f6', 'email_verified': True, 'iss': 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_kcpcLxLzn', 'cognito:username': 'f31c1cb8-681c-4d3e-9749-d7c074ffd7f6', 'aud': '77mcm1k9ll2ge68806h5kncfus', 'event_id': '1dc969c8-861e-11e8-b29e-336c6c2ce302', 'token_use': 'id', 'custom:center': 'CCMT', 'auth_time': 1531432454, 'name': 'Ashwin Ramaswami', 'exp': 1532273519, 'iat': 1532269919, 'email': 'success@simulator.amazonses.com'}
-        """
-        claims = get_claims(auth_request.token)
-        if not claims and not app.test_user_id:
-            claims = {"sub": "cm:cognitoUserPool:anonymousUser", "name": "Anonymous", "email": "anonymous@chinmayamission.com"}
-        else:
-            if claims:
-                claims["sub"] = "cm:cognitoUserPool:" + claims["sub"]
-                id = claims["sub"]
-            elif app.test_user_id:
-                claims = {"sub": app.test_user_id}
-                id = app.test_user_id
-            # try:
-            #     user = User.objects.get({"_id": id})
-            # except DoesNotExist:
-            #     print(f"User does not exist. Creating user {id}")
-            #     user = User(id=id)
-            #     user.save()
-            
-        return AuthResponse(routes=['*'], principal_id='user', context={
-            "id": claims["sub"]
-        })
+# This hack allows for integration testing.
+test_user_id = os.getenv("DEV_COGNITO_IDENTITY_ID")
+if test_user_id:
+    app.test_user_id = test_user_id
 
+@app.authorizer()
+def iamAuthorizer(auth_request):
     """
-    # Home page
-    http http://localhost:8000/forms/ "Authorization: allow"
-
-    # Get form
-    http http://localhost:8000/forms/e4548443-99da-4340-b825-3f09921b4bc5 "Authorization: allow"
-
-    http https://ewnywds4u7.execute-api.us-east-1.amazonaws.com/api/forms/ "Authorization: allow"
+    {'sub': 'f31c1cb8-681c-4d3e-9749-d7c074ffd7f6', 'email_verified': True, 'iss': 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_kcpcLxLzn', 'cognito:username': 'f31c1cb8-681c-4d3e-9749-d7c074ffd7f6', 'aud': '77mcm1k9ll2ge68806h5kncfus', 'event_id': '1dc969c8-861e-11e8-b29e-336c6c2ce302', 'token_use': 'id', 'custom:center': 'CCMT', 'auth_time': 1531432454, 'name': 'Ashwin Ramaswami', 'exp': 1532273519, 'iat': 1532269919, 'email': 'success@simulator.amazonses.com'}
     """
-
-    # app.route('/centers', methods=['GET', 'POST'], cors=True, authorizer=iamAuthorizer)(routes.center_list)
-    # app.route('/centers/{centerId}/forms', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_list)
-    # app.route('/centers/{centerId}/schemas', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.schema_list)
-    app.route('/forms', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_list)
-    app.route('/forms', methods=['POST'], cors=True, authorizer=iamAuthorizer)(routes.form_create)
-    app.route('/forms/{formId}', methods=['DELETE'], cors=True, authorizer=iamAuthorizer)(routes.form_delete)
-    app.route('/forms/{formId}', methods=['PATCH'], cors=True, authorizer=iamAuthorizer)(routes.form_edit)
-    app.route('/forms/{formId}/groups', methods=['PUT'], cors=True, authorizer=iamAuthorizer)(routes.group_edit)
-    app.route('/forms/{formId}/responses', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_response_list)
-    # form response edit
-
-    # todo use export in client side.
-    app.route('/forms/{formId}/responsesExport', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_response_export)
-    app.route('/forms/{formId}/summary', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_response_summary)
-    # app.route('/responses/{responseId}/checkin', methods=['POST'], cors=True, authorizer=iamAuthorizer)(routes.response_checkin)
-    app.route('/forms/{formId}/permissions', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_get_permissions)
-    app.route('/forms/{formId}/permissions', methods=['POST'], cors=True, authorizer=iamAuthorizer)(routes.form_edit_permissions)
-
-    app.route('/forms/{formId}', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_render)
-    app.route('/forms/{formId}/response', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_render_response)
-    app.route('/forms/{formId}', methods=['POST'], cors=True, authorizer=iamAuthorizer)(routes.form_response_new)
-    app.route('/responses/{responseId}', methods=['PATCH'], cors=True, authorizer=iamAuthorizer)(routes.response_edit)
-    app.route('/responses/{responseId}', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.response_view)
-    app.route('/responses/{responseId}/payment', methods=['POST'], cors=True, authorizer=iamAuthorizer)(routes.response_payment)
-    app.route('/responses/{responseId}', methods=['DELETE'], cors=True, authorizer=iamAuthorizer)(routes.response_delete)
-
-
-    # Unauthorized:
-    app.route('/responses/{responseId}/ipn', methods=['POST'], cors=True, content_types=['application/x-www-form-urlencoded'])(routes.response_ipn_listener)
-    app.route('/responses/{responseId}/ccavenueResponseHandler', methods=['POST'], cors=True, content_types=['application/x-www-form-urlencoded'])(routes.response_ccavenue_response_handler)
-    app.route('/responses/{responseId}/sendConfirmationEmail', methods=['POST'], cors=True)(routes.response_send_confirmation_email)
-    app.route('/confirmSignUp', methods=['GET'], cors=True)(routes.confirm_sign_up)
-
-    @app.route('/authorize', methods=['POST'], cors=True)
-    def authorize():
-        token = app.current_request.json_body["token"]
-        app_client_id = app.current_request.json_body.get("app_client_id", "")
-        if app_client_id:
-            claims = get_claims(token, verify_audience=True, app_client_id=app_client_id)
-        else:
-            claims = get_claims(token, verify_audience=True)
+    claims = get_claims(auth_request.token)
+    if not claims and not app.test_user_id:
+        claims = {"sub": "cm:cognitoUserPool:anonymousUser", "name": "Anonymous", "email": "anonymous@chinmayamission.com"}
+    else:
         if claims:
-            return claims
-        else:
-            raise BadRequestError(f"Bad request, token not valid. {claims}")
-    return app
-    
-def mocked_requests_get(*args, **kwargs):
-    class MockResponse:
-        def __init__(self, json_data, status_code):
-            self.json_data = json_data
-            self.status_code = status_code
+            claims["sub"] = "cm:cognitoUserPool:" + claims["sub"]
+            id = claims["sub"]
+        elif app.test_user_id:
+            claims = {"sub": app.test_user_id}
+            id = app.test_user_id
+        # try:
+        #     user = User.objects.get({"_id": id})
+        # except DoesNotExist:
+        #     print(f"User does not exist. Creating user {id}")
+        #     user = User(id=id)
+        #     user.save()
+        
+    return AuthResponse(routes=['*'], principal_id='user', context={
+        "id": claims["sub"]
+    })
 
-        def json(self):
-            return self.json_data
+"""
+# Home page
+http http://localhost:8000/forms/ "Authorization: allow"
 
-    if args[0] == 'http://someurl.com/test.json':
-        return MockResponse({"key1": "value1"}, 200)
-    elif args[0] == 'http://someotherurl.com/anothertest.json':
-        return MockResponse({"key2": "value2"}, 200)
+# Get form
+http http://localhost:8000/forms/e4548443-99da-4340-b825-3f09921b4bc5 "Authorization: allow"
 
-    return MockResponse(None, 404)
-    
-MODE = os.getenv("MODE", "DEV")
-print("MODE IS " + MODE)
-USER_POOL_ID = os.getenv("USER_POOL_ID")
-COGNITO_CLIENT_ID = os.getenv("COGNITO_CLIENT_ID")
-S3_UPLOADS_BUCKET_NAME = os.getenv("S3_UPLOADS_BUCKET_NAME")
-app = None
-PROD = True if MODE == "PROD" else False
-if MODE != "TEST" or True:
-    app = create_app(MODE)
-else:
-    pass
-    # # TODO fix this mocking later.
-    # from unittest import mock
-    # print("MOCKING") # todo this mocking isn't working right now.
-    # fn = mongomock.patch(servers=(('localhost', 10255),))
-    # fn = mock_ses(fn)
-    # # fn = mock.patch('requests.get', side_effect=mocked_requests_get)(fn)
-    # app = fn(create_app)(MODE)
+http https://ewnywds4u7.execute-api.us-east-1.amazonaws.com/api/forms/ "Authorization: allow"
+"""
+
+# app.route('/centers', methods=['GET', 'POST'], cors=True, authorizer=iamAuthorizer)(routes.center_list)
+# app.route('/centers/{centerId}/forms', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_list)
+# app.route('/centers/{centerId}/schemas', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.schema_list)
+app.route('/forms', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_list)
+app.route('/forms', methods=['POST'], cors=True, authorizer=iamAuthorizer)(routes.form_create)
+app.route('/forms/{formId}', methods=['DELETE'], cors=True, authorizer=iamAuthorizer)(routes.form_delete)
+app.route('/forms/{formId}', methods=['PATCH'], cors=True, authorizer=iamAuthorizer)(routes.form_edit)
+app.route('/forms/{formId}/groups', methods=['PUT'], cors=True, authorizer=iamAuthorizer)(routes.group_edit)
+app.route('/forms/{formId}/responses', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_response_list)
+# form response edit
+
+# todo use export in client side.
+app.route('/forms/{formId}/responsesExport', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_response_export)
+app.route('/forms/{formId}/summary', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_response_summary)
+# app.route('/responses/{responseId}/checkin', methods=['POST'], cors=True, authorizer=iamAuthorizer)(routes.response_checkin)
+app.route('/forms/{formId}/permissions', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_get_permissions)
+app.route('/forms/{formId}/permissions', methods=['POST'], cors=True, authorizer=iamAuthorizer)(routes.form_edit_permissions)
+
+app.route('/forms/{formId}', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_render)
+app.route('/forms/{formId}/response', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.form_render_response)
+app.route('/forms/{formId}', methods=['POST'], cors=True, authorizer=iamAuthorizer)(routes.form_response_new)
+app.route('/responses/{responseId}', methods=['PATCH'], cors=True, authorizer=iamAuthorizer)(routes.response_edit)
+app.route('/responses/{responseId}', methods=['GET'], cors=True, authorizer=iamAuthorizer)(routes.response_view)
+app.route('/responses/{responseId}/payment', methods=['POST'], cors=True, authorizer=iamAuthorizer)(routes.response_payment)
+app.route('/responses/{responseId}', methods=['DELETE'], cors=True, authorizer=iamAuthorizer)(routes.response_delete)
+
+
+# Unauthorized:
+app.route('/responses/{responseId}/ipn', methods=['POST'], cors=True, content_types=['application/x-www-form-urlencoded'])(routes.response_ipn_listener)
+app.route('/responses/{responseId}/ccavenueResponseHandler', methods=['POST'], cors=True, content_types=['application/x-www-form-urlencoded'])(routes.response_ccavenue_response_handler)
+app.route('/responses/{responseId}/sendConfirmationEmail', methods=['POST'], cors=True)(routes.response_send_confirmation_email)
+app.route('/confirmSignUp', methods=['GET'], cors=True)(routes.confirm_sign_up)
+
+@app.route('/authorize', methods=['POST'], cors=True)
+def authorize():
+    token = app.current_request.json_body["token"]
+    app_client_id = app.current_request.json_body.get("app_client_id", "")
+    if app_client_id:
+        claims = get_claims(token, verify_audience=True, app_client_id=app_client_id)
+    else:
+        claims = get_claims(token, verify_audience=True)
+    if claims:
+        return claims
+    else:
+        raise BadRequestError(f"Bad request, token not valid. {claims}")
