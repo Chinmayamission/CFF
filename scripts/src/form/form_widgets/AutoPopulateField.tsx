ab23f440774b1f7bbcaa672601dcc6a7216aa850
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-07-10 16:17:13 +0000

    autopopulate temp fixes

--- scripts/src/form/form_widgets/AutoPopulateField.tsx
+++ scripts/src/form/form_widgets/AutoPopulateField.tsx
@@ -7,77 +7,100 @@ import "./AutoPopulateField.scss";
 
 const cache = {};
 class AutoPopulateField extends React.Component<any, any> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            loading: true,
-            error: ""
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: ""
+    };
+  }
+  async componentDidMount() {
+    try {
+      let titleAccessor = this.props.uiSchema["ui:options"][
+        "cff:autoPopulateTitleAccessor"
+      ];
+      let endpoint = this.props.uiSchema["ui:options"][
+        "cff:autoPopulateEndpoint"
+      ];
+      let results = cache[endpoint];
+      if (!results) {
+        results = await fetch(endpoint).then(e => e.json());
+        // cache[endpoint] = results;
+      }
+      let options: any = [];
+      // options.push(
+      //     {"title": this.props.uiSchema["ui:placeholder"] || `Select ${this.props.name}`, "type": "object", "properties": {"a": {"type": "string"}}, "required": ["a"] }
+      // );
+      for (let result of results) {
+        let option = { type: typeof result, properties: {} };
+        if (titleAccessor) {
+          option["title"] = get(result, titleAccessor);
         }
-    }
-    async componentDidMount() {
-        try {
-            let titleAccessor = this.props.uiSchema["ui:options"]["cff:autoPopulateTitleAccessor"];
-            let endpoint = this.props.uiSchema["ui:options"]["cff:autoPopulateEndpoint"];
-            let results = cache[endpoint] || await fetch(endpoint).then(e => e.json());
-            cache[endpoint] = results;
-            let options:any = [{"title": this.props.uiSchema["ui:placeholder"] || `Select ${this.props.name}` }];
-            for (let result of results) {
-                let option = {"type": typeof result, "properties": {} };
-                if (titleAccessor) {
-                    option["title"] = get(result, titleAccessor);
-                }
-                if (this.props.schema.type === "object") {
-                    for (let key in this.props.schema.properties) {
-                        option["properties"][key] = {"type": typeof result[key], "default": result[key], "readOnly": true, "const": result[key]};
-                    }
-                }
-                // TODO: work with arrays.
-                else {
-                    option = result;
-                }
-                // TODO: change this to pass in formData in a prop when bug in rjsf is fixed -- passing formData to OneOf does not select that option, and also defaults for oneOf do not work.
-                if (!every(this.props.formData, isEmpty) && isEqual(pick(result, Object.keys(this.props.schema.properties)), this.props.formData)) {
-                    options.unshift(option);
-                }
-                else {
-                    options.push(option);
-                }
-            }
-            let newSchema = {
-                "type": "object",
-                "oneOf": options
+        if (this.props.schema.type === "object") {
+          for (let key in this.props.schema.properties) {
+            option["properties"][key] = {
+              type: typeof result[key],
+              default: result[key],
+              const: result[key]
             };
-            this.setState({
-                loading: false,
-                newSchema
-            });
+          }
         }
-        catch (e) {
-            console.error(e);
-            this.setState({
-                loading: false,
-                error: "An error occurred. Please try again later."
-            });
+        // TODO: work with arrays.
+        else {
+          option = result;
         }
+        options.push(option);
+        // TODO: change this to pass in formData in a prop when bug in rjsf is fixed -- passing formData to OneOf does not select that option, and also defaults for oneOf do not work.
+        // if (!every(this.props.formData, isEmpty) && isEqual(pick(result, Object.keys(this.props.schema.properties)), this.props.formData)) {
+        //     options.unshift(option);
+        // }
+        // else {
+        //     options.push(option);
+        // }
+      }
+      let newSchema = {
+        type: "object",
+        oneOf: options
+      };
+      this.setState({
+        loading: false,
+        newSchema
+      });
+    } catch (e) {
+      console.error(e);
+      this.setState({
+        loading: false,
+        error: "An error occurred. Please try again later."
+      });
     }
-    render() {
-        if (this.state.loading) {
-            return <Loading />;
-        }
-        if (this.state.error) {
-            return <div style={{ "color": "red" }}>{this.state.error}</div>
-        }
-        let {"ui:field": field, ...uiSchema} = this.props.uiSchema;
-        return (<div className="col-12">
-            <label className="control-label">{this.props.uiSchema["ui:title"] || this.props.schema.title || ""}</label>
-            <CustomForm
-                schema={this.state.newSchema}
-                tagName={"div"}
-                uiSchema={uiSchema}
-                formData={this.props.formData}
-                className={"ccmt-cff-Page-SubFormPage-AutoPopulate"}
-                onChange={e => this.props.onChange(e.formData)}>&nbsp;</CustomForm>
-        </div>);
+  }
+  render() {
+    if (this.state.loading) {
+      return <Loading />;
     }
-};
+    if (this.state.error) {
+      return <div style={{ color: "red" }}>{this.state.error}</div>;
+    }
+    let { "ui:field": field, ...uiSchema } = this.props.uiSchema;
+    uiSchema["ui:order"] = [...(uiSchema["ui:order"] || []), "*"];
+    console.log("FD", this.props.formData, this.state.newSchema.oneOf[0]);
+    return (
+      <div className="col-12">
+        <label className="control-label">
+          {this.props.uiSchema["ui:title"] || this.props.schema.title || ""}
+        </label>
+        <CustomForm
+          schema={this.state.newSchema}
+          tagName={"div"}
+          uiSchema={uiSchema}
+          formData={this.props.formData}
+          className={"ccmt-cff-Page-SubFormPage-AutoPopulate"}
+          onChange={e => this.props.onChange(e.formData)}
+        >
+          &nbsp;
+        </CustomForm>
+      </div>
+    );
+  }
+}
 export default AutoPopulateField;
