d9209f274ea2eb63560fc483fa6f95e5946009e7
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-07-15 17:33:03 +0000

    fix: remove hack for om run

--- scripts/src/form/CustomForm.tsx
+++ scripts/src/form/CustomForm.tsx
@@ -89,20 +89,6 @@ function validate(formData, errors, validationSchema) {
       errors.addError(item["then"]);
     }
   }
-  if (
-    formData.participants &&
-    formData.participants.length &&
-    formData.participants[0] &&
-    formData.participants[0].race
-  ) {
-    for (let participant of formData.participants) {
-      if (participant.race === "Half Marathon" && participant.age < 13) {
-        errors.participants.addError(
-          "Participants under age 13 cannot register for Half Marathon."
-        );
-      }
-    }
-  }
   // if (errors && errors.length) {
   //   alert("Errors: \n" + errors.map(e => e.stack).join("\n"));
   // }
