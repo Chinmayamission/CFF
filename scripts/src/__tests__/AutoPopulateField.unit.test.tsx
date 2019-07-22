fe22548c509869a891bb38f577f78c8c156df7b7
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-07-22 22:02:55 +0000

    refactor autopopulatefield to be testable

---
+++ scripts/src/__tests__/AutoPopulateField.unit.test.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import CustomForm from "../form/CustomForm";
+import { getSchemaFromResults } from "../form/form_widgets/AutoPopulateField";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      document: Document;
+      window: Window;
+      navigator: Navigator;
+      fetch: any;
+    }
+  }
+}
+
+// afterEach(() => {
+//     if (global.fetch) {
+//         global.fetch.mockClear();
+//         delete global.fetch;
+//     }
+// });
+
+// function mockFetch(mockSuccessResponse) {
+//     const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
+//     const mockFetchPromise = Promise.resolve({ // 3
+//         json: () => mockJsonPromise,
+//     });
+//     global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+// }
+
+describe("getSchemaFromResults", () => {
+  it("base case", () => {
+    let results = [
+      {
+        address1: "A",
+        city: "City",
+        state: "State",
+        zip: "Zipcode",
+        name: "Center Name 1"
+      }
+    ];
+    let schema = {
+      type: "object",
+      properties: {
+        name: { type: "string" },
+        address1: { type: "string" },
+        state: { type: "string" },
+        zip: { type: "string" }
+      }
+    };
+    let titleAccessor = "name";
+    expect(
+      getSchemaFromResults({ results, schema, titleAccessor })
+    ).toMatchSnapshot();
+  });
+});
