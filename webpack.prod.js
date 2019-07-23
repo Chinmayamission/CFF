a314da95b8f237a956cdffdfb067ed06b7f1b14a
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-07-23 01:15:28 +0000

    feat: update endpoint url

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,12 @@
-const merge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const common = require('./webpack.common.js');
-const path = require('path')
-const webpack = require('webpack')
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const common = require("./webpack.common.js");
+const path = require("path");
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 // const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-var pjson = require('./package.json');
+var pjson = require("./package.json");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const FORMBUILDER_URL = "./scripts";
@@ -14,24 +14,24 @@ const DEST_URL = FORMBUILDER_URL + "/prod";
 
 const MODE = "prod";
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   output: {
     path: path.resolve(DEST_URL),
-    publicPath: '/',
+    publicPath: "/",
     filename: `cff.[name].${pjson.version}.js`
   },
   plugins: [
     new CleanWebpackPlugin([DEST_URL]),
     new webpack.DefinePlugin({
-        MODE: `"${MODE}"`,
-        ENDPOINT_URL: `"https://xpqeqfjgwd.execute-api.us-east-1.amazonaws.com/v2/"`,
-        USER_POOL_ID: `"us-east-1_kcpcLxLzn"`,
-        COGNITO_CLIENT_ID: `"77mcm1k9ll2ge68806h5kncfus"`,
-        GA_TRACKING_ID: `"UA-28518772-9"`
+      MODE: `"${MODE}"`,
+      ENDPOINT_URL: `"https://forms.chinmayamission.com/api/"`,
+      USER_POOL_ID: `"us-east-1_kcpcLxLzn"`,
+      COGNITO_CLIENT_ID: `"77mcm1k9ll2ge68806h5kncfus"`,
+      GA_TRACKING_ID: `"UA-28518772-9"`
     }),
     new HtmlWebpackPlugin({
-      title: 'Chinmaya Forms Framework',
-      template: './scripts/src/index.html',
+      title: "Chinmaya Forms Framework",
+      template: "./scripts/src/index.html",
       filename: `index.${pjson.version}.html`
     })
   ]
