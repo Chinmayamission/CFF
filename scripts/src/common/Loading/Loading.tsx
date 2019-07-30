ae7bff4fc0a191d83a81d71f8eeb6e5a52676cdb
PriyalG <PriyalG@CCMT.local>
2019-07-30 10:07:23 +0530

    Changed loading icon (#144)

--- scripts/src/common/Loading/Loading.tsx
+++ scripts/src/common/Loading/Loading.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
 import "./Loading.scss";
+import { css } from "@emotion/core";
 import Modal from "react-responsive-modal";
 import { ILoadingProps, ILoadingState } from "./Loading.d";
+import FadeLoader from "react-spinners/FadeLoader";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 class Loading extends React.Component<ILoadingProps, ILoadingState> {
   constructor(props: any) {
@@ -36,25 +44,13 @@ class Loading extends React.Component<ILoadingProps, ILoadingState> {
     return (
       <div className="ccmt-cff-loading-container">
         <div className="ccmt-cff-loading">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-            <path
-              opacity=".25"
-              d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4"
-            ></path>
-            <path
-              d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z"
-              transform="rotate(152.678 16 16)"
-            >
-              <animateTransform
-                attributeName="transform"
-                type="rotate"
-                from="0 16 16"
-                to="360 16 16"
-                dur="5.3s"
-                repeatCount="indefinite"
-              ></animateTransform>
-            </path>
-          </svg>
+          <FadeLoader
+            css={override}
+            sizeUnit={"px"}
+            size={150}
+            color={"black"}
+            loading={true}
+          />
         </div>
       </div>
     );
