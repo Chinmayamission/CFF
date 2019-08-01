04fc39c64813df80b4775ee5eac5f58c94c82d04
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-08-01 17:23:01 +0000

    fix: don't set viewport

--- e2e/index.test.js
+++ e2e/index.test.js
@@ -5,7 +5,7 @@ expect.extend({ toMatchImageSnapshot });
 describe("Google", () => {
   beforeAll(async () => {
     await page.goto("http://localhost:8000");
-    await page.setViewport({ width: 1280, height: 720 });
+    // await page.setViewport({ width: 1280, height: 720 });
   });
 
   it("initial login page", async () => {
