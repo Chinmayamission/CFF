864b608185fc6d5bf63c77c3b81c687ef6e0efc9
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-09-14 14:26:44 +0000

    Refactor form response list endpoint

--- lambda/chalicelib/routes/formResponseList.py
+++ lambda/chalicelib/routes/formResponseList.py
@@ -5,6 +5,102 @@ from pydash.objects import get
 from bson.json_util import dumps
 import json
 
+def _all(form):
+    responses = Response.objects.all()._collection.find(
+        {"_cls": "chalicelib.models.Response", "form": form.id},
+        {
+            "value": 1,
+            "_id": 1,
+            "amount_paid": 1,
+            "user": 1,
+            "form": 1,
+            "paymentInfo": 1,
+            "date_created": 1,
+            "date_modified": 1,
+            "paid": 1,
+            "counter": 1,
+        },
+    )
+    return {"res": [r for r in json.loads(dumps(responses))]}
+
+def _dataOption():
+    return {}
+
+def _search(form, query, autocomplete, search_by_id, show_unpaid):
+    search_fields = get(
+        form.formOptions.dataOptions, "search.searchFields", ["_id"]
+    )
+    if search_by_id is not None:
+        search_fields = ["_id"]
+    result_limit = get(form.formOptions.dataOptions, "search.resultLimit", 10)
+    result_fields = get(
+        form.formOptions.dataOptions, "search.resultFields", ["_id"]
+    )
+    autocomplete_fields = get(
+        form.formOptions.dataOptions, "search.autocompleteFields", ["_id"]
+    )
+    if show_unpaid is not None:
+        default_mongo_query = {"paid": False}
+    else:
+        default_mongo_query = {"paid": True}
+    mongo_query = {"$or": []}
+    for word in query.split(" "):
+        for field in search_fields:
+            if field == "_id":
+                if len(word) <= 24:
+                    try:
+                        queryObjectIdStart = ObjectId(
+                            word + "0" * (24 - len(word))
+                        )  # fill in zeroes to create object id, e.g. 5cba --> 5cba0000000000000000000
+                        queryObjectIdEnd = ObjectId(word + "e" * (24 - len(word)))
+                        mongo_query["$or"].append(
+                            {
+                                field: {
+                                    "$gte": queryObjectIdStart,
+                                    "$lte": queryObjectIdEnd,
+                                }
+                            }
+                        )
+                    except bson.errors.InvalidId:
+                        pass
+            else:
+                if field.startswith("value.participants."):
+                    _, subfield = field.split("value.participants.")
+                    mongo_query["$or"].append(
+                        {
+                            "value.participants": {
+                                "$elemMatch": {
+                                    subfield: {
+                                        "$regex": "^" + word,
+                                        "$options": "i",
+                                    }
+                                }
+                            }
+                        }
+                    )
+                else:
+                    mongo_query["$or"].append(
+                        {field: {"$regex": "^" + word, "$options": "i"}}
+                    )
+    mongo_query["form"] = form.id
+    if len(mongo_query["$or"]) == 0:
+        del mongo_query["$or"]
+    # Default query paid = True
+    if mongo_query:
+        mongo_query = {"$and": [default_mongo_query, mongo_query]}
+    else:
+        mongo_query = default_mongo_query
+    if autocomplete is not None:
+        projection = {field: 1 for field in autocomplete_fields}
+        result_limit = 5
+    else:
+        projection = {}
+        for field in result_fields:
+            projection[field] = 1
+    responses = (
+        Response.objects.raw(mongo_query).limit(result_limit).project(projection)
+    )
+    return {"res": [serialize_model(r) for r in responses]}
 
 def form_response_list(formId):
     """Show all responses for a particular form.
@@ -18,114 +114,19 @@ def form_response_list(formId):
     form = Form.objects.only("formOptions", "cff_permissions").get(
         {"_id": ObjectId(formId)}
     )
-    # todo: use search framework, don't return all!
-    query = app.current_request.query_params and app.current_request.query_params.get(
-        "query", None
-    )
-    autocomplete = (
-        app.current_request.query_params
-        and app.current_request.query_params.get("autocomplete", None)
-    )
-    search_by_id = (
-        app.current_request.query_params
-        and app.current_request.query_params.get("search_by_id", None)
-    )
-    show_unpaid = (
-        app.current_request.query_params
-        and app.current_request.query_params.get("show_unpaid", None)
-    )
-    if query:
-        # autocomplete, participant name, assign bibs functionality
+    query_params = app.current_request.query_params or {}
+    
+    query = query_params.get("query", None)
+    dataOption = query_params.get("dataOption", None)
+    if dataOption:
+        app.check_permissions(form, ["Responses_View"])
+        return _dataOption(dataOption)
+    elif query:
         app.check_permissions(form, ["Responses_View", "Responses_CheckIn"])
-        search_fields = get(
-            form.formOptions.dataOptions, "search.searchFields", ["_id"]
-        )
-        if search_by_id is not None:
-            search_fields = ["_id"]
-        result_limit = get(form.formOptions.dataOptions, "search.resultLimit", 10)
-        result_fields = get(
-            form.formOptions.dataOptions, "search.resultFields", ["_id"]
-        )
-        autocomplete_fields = get(
-            form.formOptions.dataOptions, "search.autocompleteFields", ["_id"]
-        )
-        if show_unpaid is not None:
-            default_mongo_query = {"paid": False}
-        else:
-            default_mongo_query = {"paid": True}
-        mongo_query = {"$or": []}
-        for word in query.split(" "):
-            for field in search_fields:
-                if field == "_id":
-                    if len(word) <= 24:
-                        try:
-                            queryObjectIdStart = ObjectId(
-                                word + "0" * (24 - len(word))
-                            )  # fill in zeroes to create object id, e.g. 5cba --> 5cba0000000000000000000
-                            queryObjectIdEnd = ObjectId(word + "e" * (24 - len(word)))
-                            mongo_query["$or"].append(
-                                {
-                                    field: {
-                                        "$gte": queryObjectIdStart,
-                                        "$lte": queryObjectIdEnd,
-                                    }
-                                }
-                            )
-                        except bson.errors.InvalidId:
-                            pass
-                else:
-                    if field.startswith("value.participants."):
-                        _, subfield = field.split("value.participants.")
-                        mongo_query["$or"].append(
-                            {
-                                "value.participants": {
-                                    "$elemMatch": {
-                                        subfield: {
-                                            "$regex": "^" + word,
-                                            "$options": "i",
-                                        }
-                                    }
-                                }
-                            }
-                        )
-                    else:
-                        mongo_query["$or"].append(
-                            {field: {"$regex": "^" + word, "$options": "i"}}
-                        )
-        mongo_query["form"] = form.id
-        if len(mongo_query["$or"]) == 0:
-            del mongo_query["$or"]
-        # Default query paid = True
-        if mongo_query:
-            mongo_query = {"$and": [default_mongo_query, mongo_query]}
-        else:
-            mongo_query = default_mongo_query
-        if autocomplete is not None:
-            projection = {field: 1 for field in autocomplete_fields}
-            result_limit = 5
-        else:
-            projection = {}
-            for field in result_fields:
-                projection[field] = 1
-        responses = (
-            Response.objects.raw(mongo_query).limit(result_limit).project(projection)
-        )
+        autocomplete = query_params.get("autocomplete", None)
+        search_by_id = query_params.get("search_by_id", None)
+        show_unpaid = query_params.get("show_unpaid", None)
+        return _search(form, query, autocomplete, search_by_id, show_unpaid)
     else:
         app.check_permissions(form, ["Responses_View"])
-        responses = Response.objects.all()._collection.find(
-            {"_cls": "chalicelib.models.Response", "form": form.id},
-            {
-                "value": 1,
-                "_id": 1,
-                "amount_paid": 1,
-                "user": 1,
-                "form": 1,
-                "paymentInfo": 1,
-                "date_created": 1,
-                "date_modified": 1,
-                "paid": 1,
-                "counter": 1,
-            },
-        )
-        return {"res": [r for r in json.loads(dumps(responses))]}
-    return {"res": [serialize_model(r) for r in responses]}
+        return _all(form)
