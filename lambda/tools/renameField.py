f797d318847ecc22a77cc3dda1cefb3cf87ed420
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-10-22 19:47:25 +0000

    update renameField tool

--- lambda/tools/renameField.py
+++ lambda/tools/renameField.py
@@ -1,14 +1,5 @@
-from chalicelib.models import (
-    Response,
-    Form,
-    PaymentTrailItem,
-    PaymentStatusDetailItem,
-    serialize_model,
-)
-from chalicelib.main import app, MODE
-
 """
-pipenv run python -m unittest tools.renameField
+MODE=PROD AWS_PROFILE=ashwin-cff-lambda pipenv run python -m unittest tools.renameField
 Renames fields
 """
 import os
@@ -22,22 +13,28 @@ from decimal import Decimal
 from pydash.arrays import find_index
 
 os.environ["AWS_PROFILE"] = "ashwin-cff-lambda"
-os.environ["MODE"] = "BETA"
+os.environ["MODE"] = "PROD"
 os.environ["DB_NAME"] = "cff_prod"
 os.environ["USER_POOL_ID"] = ""
 os.environ["COGNITO_CLIENT_ID"] = ""
 
+from chalicelib.models import (
+    Response,
+    Form,
+    PaymentTrailItem,
+    PaymentStatusDetailItem,
+    serialize_model,
+)
+from chalicelib.main import app, MODE
 
 print("MODE", MODE)
 
-# responses = Response.objects.raw({"form": ObjectId("5b3b4e7fb1117c0001c138ed")})
+responses = Response.objects.raw({"form": ObjectId("5c96811ed0443d00011255d5")})
+# print(len(responses))
 # for response in responses:
 #   print("res")
 
-# for response in responses:
-#   if response.value["parivar"] == True:
-#     response.value["membership_type"] = "parivar_monthly"
-#   elif response.value["parivar"] == False:
-#     response.value["membership_type"] = "basic"
-#   del response.value["parivar"]
-#   response.save()
+for response in responses:
+  response.value["decided"] = "decided"
+  print(response.id)
+  response.save()
