4993a4356086b3a41a48f478842fcf0e25986737
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-03-15 19:59:27 +0000

    use absolute paths for wkhtmltopdf, add to /bin directory

--- lambda/chalicelib/util/pdf_generator.py
+++ lambda/chalicelib/util/pdf_generator.py
@@ -10,9 +10,13 @@ def convert_html_to_pdf(source):
     """Converts a HTML string to a PDF file, then returns the binary contents of that file.
     """
     config = pdfkit.configuration(
-        wkhtmltopdf="vendor/wkhtmltopdf"
+        wkhtmltopdf=os.path.abspath(
+            os.path.join(__file__, "../../../vendor/bin/wkhtmltopdf")
+        )
         if MODE in ("DEV", "TEST")
-        else "wkhtmltopdf"  # vendor/ directory gets moved to working directory after chalice deployment
+        else os.path.abspath(
+            os.path.join(__file__, "../../../bin/wkhtmltopdf")
+        )  # vendor/ directory gets moved to top directory after chalice deployment
     )
     pdfkit.from_string(source, TEMP_PDF_FILENAME, configuration=config)
     with open(TEMP_PDF_FILENAME, "rb") as f:
--- lambda/scripts/install-deps.sh
+++ lambda/scripts/install-deps.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y openssl build-essential xorg libssl-dev
 wget "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/${WKHTML2PDF_VERSION}/wkhtmltox-${WKHTML2PDF_VERSION}_linux-generic-amd64.tar.xz"
 tar -xJf "wkhtmltox-${WKHTML2PDF_VERSION}_linux-generic-amd64.tar.xz"
 chmod +x wkhtmltox/bin/wkhtmltopdf
-cp wkhtmltox/bin/wkhtmltopdf vendor
+cp wkhtmltox/bin/wkhtmltopdf vendor/bin
 rm -r wkhtmltox "wkhtmltox-${WKHTML2PDF_VERSION}_linux-generic-amd64.tar.xz"
 # sudo chown root:root bin/wkhtmltopdf
 # sudo cp -r * /usr/
