30c96ec7435cb4674d8d741bfca50705f2feeaf3
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-04-26 19:01:32 -0400

    remove unnecessary jwt logs

--- lambda/chalicelib/util/jwt.py
+++ lambda/chalicelib/util/jwt.py
@@ -51,9 +51,7 @@ def get_claims(
     decoded_signature = base64url_decode(encoded_signature.encode("utf-8"))
     # verify the signature
     if not public_key.verify(message.encode("utf8"), decoded_signature):
-        print("Signature verification failed")
         return False
-    print("Signature successfully verified")
     # since we passed the verification, we can now safely
     # use the unverified claims
     claims = jwt.get_unverified_claims(token)
