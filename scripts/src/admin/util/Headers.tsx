74aacbbf131a077de952838b6d04df14649c6cff
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-02-13 14:20:30 -0500

    Responses table view: Allow groupEdit to be a text box (not just a select box) (#252)
    * Responses table view: Allow groupEdit to be a text box (not just a select box)
    
    * Update snapshots

--- scripts/src/admin/util/Headers.tsx
+++ scripts/src/admin/util/Headers.tsx
@@ -8,6 +8,7 @@ import { filterCaseInsensitive } from "../ResponseTable/filters";
 import { dataToSchemaPath } from "../util/SchemaUtil";
 import ExpressionParser from "../../common/ExpressionParser";
 import moment from "moment";
+import CustomForm from "../../form/CustomForm";
 
 export interface IHeaderObject {
   Header: string;
@@ -395,11 +396,16 @@ export namespace Headers {
           .map(e => formatValue(e))
       };
     }
+    const hasEnum = !!selectSchema.enum;
     headerObj.headerClassName = "ccmt-cff-no-click";
     headerObj.Cell = row => (
-      <Form
+      <CustomForm
         schema={selectSchema}
-        uiSchema={{ "ui:widget": "select" }}
+        uiSchema={
+          hasEnum
+            ? { "ui:widget": "select" }
+            : { "ui:widget": "cff:submitInputGroup" }
+        }
         formData={row.value}
         onChange={e => {
           if (typeof e.formData === "undefined") return;
@@ -412,16 +418,24 @@ export namespace Headers {
         }}
         tagName="div"
       >
-        <div className="d-none"></div>
-      </Form>
+        <></>
+      </CustomForm>
     );
     headerObj.filterMethod = filterMethodAllNone;
     let selectSchemaFilter = cloneDeep(selectSchema);
-    selectSchemaFilter.enum.unshift("CFF_FILTER_NONE");
-    selectSchemaFilter.enumNames.unshift("None");
-    selectSchemaFilter.enum.unshift("CFF_FILTER_DEFINED");
-    selectSchemaFilter.enumNames.unshift("Defined");
+    if (selectSchemaFilter.enum) {
+      selectSchemaFilter.enum.unshift("CFF_FILTER_NONE");
+      selectSchemaFilter.enum.unshift("CFF_FILTER_DEFINED");
+    }
+    if (selectSchemaFilter.enumNames) {
+      selectSchemaFilter.enumNames.unshift("None");
+      selectSchemaFilter.enumNames.unshift("Defined");
+    }
     headerObj.Filter = ({ filter, onChange }) => {
+      if (!hasEnum) {
+        // No filter if there is no select schema
+        return null;
+      }
       if (!filter && headerOption.defaultFilter) {
         class Loader extends React.Component {
           componentDidMount() {
---
+++ scripts/src/form/form_widgets/SubmitInputGroupWidget.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Form from "react-jsonschema-form";
+import "./SubmitInputGroupWidget.scss";
+
+export default ({ schema, uiSchema, value, onChange, id }) => {
+  const [data, setData] = useState(value);
+  const [editing, setEditing] = useState(false);
+
+  if (editing) {
+    return (
+      <div className="cff-submitinputgroupwidget">
+        <Form
+          tagName="div"
+          schema={schema}
+          uiSchema={uiSchema}
+          formData={data}
+          onChange={e => e.formData !== undefined && setData(e.formData)}
+          idPrefix={id + "_widget"}
+        >
+          <></>
+        </Form>
+        <span
+          className="oi oi-check icon"
+          onClick={() => onChange(data)}
+        ></span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="cff-submitinputgroupwidget">
+        {value}
+        <span
+          className="oi oi-pencil icon"
+          onClick={() => setEditing(true)}
+        ></span>
+      </div>
+    );
+  }
+};
