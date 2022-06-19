49d1e6d552ae8cf9aed7e9dd25311cfa1b09e6fe
Ashwin Ramaswami <aramaswamis@gmail.com>
2022-06-19 14:51:49 -0400

    Update FileInputAndPreviewWidget.tsx

--- scripts/src/form/form_widgets/FileInputAndPreviewWidget.tsx
+++ scripts/src/form/form_widgets/FileInputAndPreviewWidget.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import FileWidget from "@rjsf/core/lib/components/widgets/FileWidget";
+const FileWidget = require("@rjsf/core/lib/components/widgets/FileWidget");
 
 /*
 From https://github.com/TreeHacks/application-portal-frontend/blob/4d5c542498499c18bba1d0e3cfded0ff50964464/src/FormPage/FormPage.tsx#L64
