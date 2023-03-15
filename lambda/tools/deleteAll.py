1aedbcb776856e54bdccd7ae65506654d21f2dfb
Ashwin Ramaswami <aramaswamis@gmail.com>
2023-03-15 03:05:26 +0000

    fix deleteAll script

--- lambda/tools/deleteAll.py
+++ lambda/tools/deleteAll.py
@@ -1,12 +1,3 @@
-from chalicelib.models import (
-    Response,
-    Form,
-    PaymentTrailItem,
-    PaymentStatusDetailItem,
-    serialize_model,
-)
-from chalicelib.main import app, MODE
-
 """
 pipenv run python -m unittest tools.deleteAll
 Deletes all responses for a particular form.
@@ -23,19 +14,31 @@ from pydash.arrays import find_index
 
 PROD = True
 
-formId = "" if PROD else "...."
-
-os.environ["AWS_PROFILE"] = "default"
 os.environ["MODE"] = "PROD"
 os.environ["DB_NAME"] = "cff_prod" if PROD else "cff_beta"
 os.environ["USER_POOL_ID"] = ""
 os.environ["S3_UPLOADS_BUCKET_NAME"] = ""
 os.environ["COGNITO_CLIENT_ID"] = ""
 
+from chalicelib.models import (
+    Response,
+    Form,
+    PaymentTrailItem,
+    PaymentStatusDetailItem,
+    serialize_model,
+)
+from chalicelib.main import app, MODE
+
+formId = "6255723a0435df8bab9961fe" if PROD else "...."
+
 
 print("MODE", MODE)
 
-responses = Response.objects.raw({"form": ObjectId(formId)})
+responses = Response.objects.raw({"form": ObjectId("")})
+
+# responses = Response.objects.raw({})
+
+# print(len(list(responses)))
 
 for response in responses:
     response.delete()
