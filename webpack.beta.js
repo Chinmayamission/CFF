b5e839f669f4e8e157f8c5332a9f9357269714c7
Ashwin Ramaswami <aramaswamis@gmail.com>
2023-05-02 22:12:21 +0000

    Update key

--- webpack.beta.js
+++ webpack.beta.js
@@ -25,7 +25,7 @@ module.exports = version =>
         USER_POOL_ID: `"us-east-1_U9ls8R6E3"`,
         COGNITO_CLIENT_ID: `"2511g7rmn8p70losdlh9gi9j0"`,
         GA_TRACKING_ID: `"UA-28518772-10"`,
-        GOOGLE_MAPS_API_KEY: `"AIzaSyCkFt0z9cCfaXt6INQGnhXLF7wossqNYUE"`
+        GOOGLE_MAPS_API_KEY: `"AIzaSyAIJZv8VKMaV_ckEPGtQ48CZiI5z5E1rCw"`
       }),
       new HtmlWebpackPlugin({
         title: "Chinmaya Forms Framework - Beta",
