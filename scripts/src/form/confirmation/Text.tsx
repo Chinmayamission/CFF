c847d6f2273890f7e13d7ae5a48c387195e56390
Ashwin Ramaswami <aramaswamis@gmail.com>
2023-06-19 03:35:47 +0000

    alert text

--- scripts/src/form/confirmation/Text.tsx
+++ scripts/src/form/confirmation/Text.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 const Text = ({ paymentMethodInfo }) => {
-  return <div>{paymentMethodInfo.text}</div>;
+  return <div className="alert alert-info">{paymentMethodInfo.text}</div>;
 };
 
 export default Text;
